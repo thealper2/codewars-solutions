36e5b26f74e22585565ffe626c730caad560300f
Alper Karaca <thisiseternity12@gmail.com>
2024-09-15 23:27:50 +0300

    Initial commit

---
+++ 8-kyu/convert_to_binary.py
@@ -0,0 +1,9 @@
+def to_binary(n):
+    i = n
+    result = []
+    while i >= 1:
+        a = i % 2
+        i = i // 2
+        result[0:0] = str(a)
+
+    return int(''.join(result))
---
+++ 8-kyu/for_twins_1_types.py
@@ -0,0 +1,2 @@
+def type_validation(variable, _type):
+    return variable.__class__.__name__ == _type
