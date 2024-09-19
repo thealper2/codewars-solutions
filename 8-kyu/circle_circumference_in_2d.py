c9040e124df1ddafa34f9b600c2a98e921990dbc
Alper Karaca <thisiseternity12@gmail.com>
2024-09-19 14:03:37 +0300

    Initial commit

---
+++ 8-kyu/circle_circumference_in_2d.py
@@ -0,0 +1,4 @@
+import math
+
+def circle_circumference(circle):
+    return round(2 * math.pi * circle.radius, 6)
---
+++ 8-kyu/distance_between_points_in_2d.py
@@ -0,0 +1,2 @@
+def distance_between_points(a, b):
+    return round(((b.x - a.x) ** 2 + (b.y - a.y) ** 2) ** 0.5, 6)
