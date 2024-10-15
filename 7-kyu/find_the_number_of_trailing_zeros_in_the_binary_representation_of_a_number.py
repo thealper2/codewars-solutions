c044f635e611e8cd459978a381c58f992a67c161
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2024-10-15 19:04:54 +0300

    Initial commit

---
+++ 7-kyu/find_the_number_of_trailing_zeros_in_the_binary_representation_of_a_number.py
@@ -0,0 +1,17 @@
+def trailing_zeros(n) -> int:
+    i = n
+    result = []
+    while i >= 1:
+        a = i % 2
+        i = i // 2
+        result[0:0] = str(a)
+    
+    result = result[::-1]
+    count = 0
+    for item in result:
+        if int(item) == 1:
+            break
+            
+        count += 1
+        
+    return count
---
+++ 7-kyu/prism_analyzer.py
@@ -0,0 +1,8 @@
+def prism_properties(s):
+    k = int((s - 2) / 6)
+    #s = 6k + 2
+    v = 2 * k
+    e = 3 * k
+    f = k + 2
+    b = k
+    return (v, e, f, b)
