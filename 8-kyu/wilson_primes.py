d1b16990f2608ab6ee1326594e734b7753a38e69
Alper Karaca <thisiseternity12@gmail.com>
2025-06-30 23:16:46 +0300

    Wilson primes

--- 8-kyu/wilson_primes.py
+++ 8-kyu/wilson_primes.py
@@ -1,8 +1,3 @@
-import math
-
 def am_i_wilson(n):
-    if n < 2:
-        return False
-
-    factorial_mod = math.factorial(n - 1) % (n * n)
-    return factorial_mod == (n * n - 1)
+    wilson_primes = {5, 13, 563}
+    return n in wilson_primes
