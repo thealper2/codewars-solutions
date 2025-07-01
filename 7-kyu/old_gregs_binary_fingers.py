3f33844328630c7c61cec2f168f7d872a091fc5f
Alper Karaca <thisiseternity12@gmail.com>
2025-07-01 22:22:16 +0300

    Old Greg's Binary Fingers

---
+++ 7-kyu/old_gregs_binary_fingers.py
@@ -0,0 +1,10 @@
+def binary_fingers(bin_string):
+    finger_names = ["Pinkie", "Ring", "Middle", "Index", "Thumb"]
+    result = []
+    padded_bin = bin_string.zfill(5)
+    relevant_bits = padded_bin[-5:]
+    for i in range(5):
+        if i < len(relevant_bits) and relevant_bits[i] == '1':
+            result.append(finger_names[i])
+            
+    return result
