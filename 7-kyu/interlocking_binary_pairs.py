9e65b3c4017dcd5e48b54d445852ec27c412f465
Alper Karaca <thisiseternity12@gmail.com>
2025-07-01 21:12:47 +0300

    Interlocking Binary Pairs

---
+++ 7-kyu/interlocking_binary_pairs.py
@@ -0,0 +1,19 @@
+def interlockable(a, b):
+    a_binary = []
+    b_binary = []
+    
+    while a >= 1 and b >= 1:
+        a_binary.append(a % 2)
+        a //= 2
+        
+    while b >= 1:
+        b_binary.append(b % 2)
+        b //= 2
+
+    n = min(len(a_binary), len(b_binary))
+
+    for a_, b_ in zip(a_binary[:n], b_binary[:n]):
+        if a_ == 1 and b_ == 1:
+            return False
+
+    return True
