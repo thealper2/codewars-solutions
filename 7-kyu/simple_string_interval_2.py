237f21a315acfbfaefaf722e8ef45d37298ed8c8
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 11:22:02 +0300

    Simple string interval II

---
+++ 7-kyu/simple_string_interval_2.py
@@ -0,0 +1,6 @@
+def solve(st,a,b):
+    if b >= len(st):
+        b = len(st) - 1
+    substring = st[a:b+1]
+    reversed_substring = substring[::-1]
+    return st[:a] + reversed_substring + st[b+1:]
