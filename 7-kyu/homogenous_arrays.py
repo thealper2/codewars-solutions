efc6c42152cd63851f4fa5f78af14e584e6fb0a4
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 11:33:44 +0300

    Homogenous arrays

---
+++ 7-kyu/homogenous_arrays.py
@@ -0,0 +1,17 @@
+def filter_homogenous(arrays):
+    result = []
+    for array in arrays:
+        if len(array) == 0:
+            continue
+            
+        first_type = type(array[0])
+        homogenous = True
+        for element in array[1:]:
+            if type(element) != first_type:
+                homogenous = False
+                break
+                
+        if homogenous:
+            result.append(array)
+            
+    return result
