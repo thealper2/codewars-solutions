0ff3ea8347f6f5e81adca022f03006bbffc7a1a8
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 09:24:24 +0300

    Thinking & Testing: Retention and discard

---
+++ 7-kyu/thinking_and_testing_retention_and_discard.py
@@ -0,0 +1,10 @@
+def mystery(n):
+    if n <= 0:
+        return []
+    
+    result = []
+    for i in range(1, n + 1, 2):
+        if n % i == 0:
+            result.append(i)
+            
+    return result
