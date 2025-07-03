2676ab81f7dd9d0723264e3061ae6a9a91782851
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 10:11:07 +0300

    Product Array (Array Series #5)

---
+++ 7-kyu/product_array.py
@@ -0,0 +1,17 @@
+def product_array(numbers):
+    n = len(numbers)
+    if n == 0:
+        return []
+    
+    prod = [1] * n
+    prefix = 1
+    for i in range(n):
+        prod[i] = prefix
+        prefix *= numbers[i]
+    
+    suffix = 1
+    for i in range(n-1, -1, -1):
+        prod[i] *= suffix
+        suffix *= numbers[i]
+    
+    return prod
