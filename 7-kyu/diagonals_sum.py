9d582bbf5f80f92144b1b7ffb47d280d4df59f4f
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 11:36:11 +0300

    Diagonals sum

---
+++ 7-kyu/diagonals_sum.py
@@ -0,0 +1,9 @@
+def sum_diagonals(matrix):
+    total = 0
+    n = len(matrix)
+    
+    for i in range(n):
+        total += matrix[i][i]
+        total += matrix[n-i-1][i]
+        
+    return total
