eeff4161ca4418bc1a5b258c3e3a60779bd48576
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 16:38:20 +0300

    Find needed interest rate

---
+++ 7-kyu/find_needed_interest_rate.py
@@ -0,0 +1,18 @@
+from decimal import Decimal, getcontext
+
+def required_interest_rate(initial, target, years, period):
+    if initial == target:
+        return Decimal('0')
+    
+    getcontext().prec = 28
+    
+    P = initial
+    A = target
+    t = Decimal(years)
+    n_compounding = Decimal(period)
+    exponent = 1 / (n_compounding * t)
+    ratio = A / P
+    root = ratio ** exponent
+    r = n_compounding * (root - 1)
+    r_percent = r * Decimal('100')
+    return r_percent
