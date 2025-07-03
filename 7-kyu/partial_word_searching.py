0b984c6f4a9214a4fda52412fc363cda26c02a98
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 10:14:04 +0300

    Partial Word Searching

---
+++ 7-kyu/partial_word_searching.py
@@ -0,0 +1,10 @@
+def word_search(query, seq):
+    result = []
+    for s in seq:
+        if query.lower() in s.lower():
+            result.append(s)
+            
+    if len(result) == 0:
+        result.append("None")
+            
+    return result
