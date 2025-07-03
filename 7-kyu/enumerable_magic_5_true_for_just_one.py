a8a48f086ea203ae890c21551261dc6c77db6133
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 09:51:47 +0300

    Enumerable Magic #5 - True for Just One?

---
+++ 7-kyu/enumerable_magic_5_true_for_just_one.py
@@ -0,0 +1,9 @@
+def one(xs, f):
+    count = 0
+    for item in xs:
+        if f(item):
+            count += 1
+            if count > 1:
+                return False
+            
+    return count == 1
