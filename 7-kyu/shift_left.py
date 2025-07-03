a16943ad460b1539bdc044eafa26e3bac9c2a182
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 16:41:58 +0300

    Shift Left

---
+++ 7-kyu/shift_left.py
@@ -0,0 +1,13 @@
+def shift_left(a, b):
+    len1 = len(a)
+    len2 = len(b)
+    min_len = min(len1, len2)
+    common_suffix_length = 0
+    
+    for i in range(1, min_len + 1):
+        if a[len1 - i] == b[len2 - i]:
+            common_suffix_length += 1
+        else:
+            break
+    
+    return (len1 - common_suffix_length) + (len2 - common_suffix_length)
