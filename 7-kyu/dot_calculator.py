3e1ec349b3ed0b72cc15bd943d48088836fd6a01
Alper Karaca <thisiseternity12@gmail.com>
2025-07-03 23:13:54 +0300

    Dot Calculator

---
+++ 7-kyu/dot_calculator.py
@@ -0,0 +1,16 @@
+def calculator(txt):
+    parts = txt.split()
+    a = len(parts[0])
+    operator = parts[1]
+    b = len(parts[2])
+    
+    if operator == '+':
+        return '.' * (a + b)
+    elif operator == '-':
+        return '.' * (a - b)
+    elif operator == '*':
+        return '.' * (a * b)
+    elif operator == '//':
+        return '.' * (a // b)
+    else:
+        return '.' * (a // b)
