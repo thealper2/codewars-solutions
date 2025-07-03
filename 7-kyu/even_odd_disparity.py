eb0ef7e801a0568b6c310ff12d03343655a5dd9a
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 09:53:55 +0300

    Even odd disparity

---
+++ 7-kyu/even_odd_disparity.py
@@ -0,0 +1,10 @@
+def solve(a):
+    c = 0
+    for x in a:
+        if type(x) in [float, int]:
+            if x % 2 == 0:
+                c += 1
+            else:
+                c -= 1
+                
+    return c
