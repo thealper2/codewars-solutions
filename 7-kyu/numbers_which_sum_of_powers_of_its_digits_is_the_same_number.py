69ca921ff0b1d459e07de76265af3dd8a58191a7
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 09:36:17 +0300

    Numbers Which Sum of Powers of Its Digits Is The Same Number

---
+++ 7-kyu/numbers_which_sum_of_powers_of_its_digits_is_the_same_number.py
@@ -0,0 +1,12 @@
+def eq_sum_powdig(h_max, exp):
+    result = []
+    for num in range(2, h_max + 1):
+        squared = 0
+        for d in str(num):
+            squared += int(d) ** exp
+
+        if squared == num:
+            result.append(num)
+            
+    return result
+    
