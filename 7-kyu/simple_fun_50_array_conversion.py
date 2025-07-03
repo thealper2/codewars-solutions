56ecde76cf2cc7edc696acfc3c591f962e199b72
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-03 16:26:36 +0300

    Simple Fun #50: Array Conversion

---
+++ 7-kyu/simple_fun_50_array_conversion.py
@@ -0,0 +1,19 @@
+def array_conversion(arr):
+    iteration = 1
+    while len(arr) > 1:
+        new_arr = []
+        if iteration % 2 == 1:
+            for i in range(0, len(arr), 2):
+                if i + 1 < len(arr):
+                    new_arr.append(arr[i] + arr[i+1])
+                else:
+                    new_arr.append(arr[i])
+        else:
+            for i in range(0, len(arr), 2):
+                if i + 1 < len(arr):
+                    new_arr.append(arr[i] * arr[i+1])
+                else:
+                    new_arr.append(arr[i])
+        arr = new_arr
+        iteration += 1
+    return arr[0]
