d684ebd4f2bd9595a287f5dcb6dece14384881c2
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 08:41:58 +0300

    Number of Decimal Digits

---
+++ 7-kyu/number_of_decimal_digits.py
@@ -0,0 +1,13 @@
+def digits(n):
+    if n < 0:
+        n *= -1
+        
+    if n == 0:
+        return 1
+        
+    count = 0
+    while n > 0:
+        n //= 10
+        count += 1
+        
+    return count
