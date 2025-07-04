de94563b5c890dd59bea1d217b483e43ec63e624
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 08:45:42 +0300

    The old switcheroo 2

---
+++ 7-kyu/the_old_switcheroo_2.py
@@ -0,0 +1,11 @@
+def encode(st):
+    result = ""
+    for c in st:
+        if 65 <= ord(c) <= 90:
+            result += str(ord(c) % 64)
+        elif 97 <= ord(c) <= 122:
+            result += str(ord(c) % 96)
+        else:
+            result += c
+            
+    return result
