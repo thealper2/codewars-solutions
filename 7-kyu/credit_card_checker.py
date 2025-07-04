cddd57f44f9dddb9e5fb0e037a2f454f2f9581f5
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 10:27:57 +0300

    Credit Card Checker

---
+++ 7-kyu/credit_card_checker.py
@@ -0,0 +1,9 @@
+def valid_card(card):
+    cleaned = "".join(filter(str.isdigit, card))
+    digits = [int(digit) for digit in cleaned]
+    transformed_digits = digits.copy()
+    for i in range(len(transformed_digits) - 2, -1, -2):
+        doubled = transformed_digits[i] * 2
+        transformed_digits[i] = doubled - 9 if doubled > 9 else doubled
+        
+    return sum(transformed_digits) % 10 == 0
