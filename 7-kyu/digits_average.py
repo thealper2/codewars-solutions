007a13ad2c64777d25c0bcb65980d4eba3325f53
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 11:41:28 +0300

    Digits Average

---
+++ 7-kyu/digits_average.py
@@ -0,0 +1,14 @@
+import math
+
+
+def digits_average(n):
+    digits = [int(d) for d in str(n)]
+    while len(digits) > 1:
+        new_digits = []
+        for i in range(len(digits) - 1):
+            avg = (digits[i] + digits[i+1]) / 2
+            rounded_avg = math.ceil(avg)
+            new_digits.append(rounded_avg)
+        digits = new_digits
+
+    return digits[0]
