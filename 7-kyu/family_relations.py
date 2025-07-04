157b513ab66b1fd968887b799f6079ea5f19b874
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 14:04:08 +0300

    Family Relations

---
+++ 7-kyu/family_relations.py
@@ -0,0 +1,28 @@
+def relations(family_list, target_pair):
+    parents = {}
+    for parent, child in family_list:
+        parents[child] = parent
+        
+    a, b = target_pair
+    a_parent = parents.get(a)
+    a_parent_parent = parents.get(a_parent)
+    b_parent = parents.get(b)
+    b_parent_parent = parents.get(b_parent)
+    
+    if b == a_parent:
+        return 'Mother'
+    
+    if b == a_parent_parent:
+        return 'Grandmother'
+    if a == b_parent:
+        return 'Daughter'
+    if a == b_parent_parent:
+        return 'Granddaughter'
+    if a_parent == b_parent:
+        return 'Sister'
+    if a_parent_parent == b_parent_parent:
+        return 'Cousin'
+    if a_parent_parent == b_parent:
+        return 'Aunt'
+    if a_parent == b_parent_parent:
+        return 'Niece'
