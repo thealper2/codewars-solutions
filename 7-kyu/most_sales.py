9f838826c8548fe4f76b2075c0d5173a675e15ff
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 16:55:38 +0300

    Most sales

---
+++ 7-kyu/most_sales.py
@@ -0,0 +1,8 @@
+def top3(products, amounts, prices):
+    n = len(products)
+    sales = []
+    for i in range(n):
+        sales.append((products[i], amounts[i] * prices[i], i))
+    
+    sales.sort(key=lambda x: (-x[1], x[2]))
+    return [product for product, total, index in sales[:3]]
