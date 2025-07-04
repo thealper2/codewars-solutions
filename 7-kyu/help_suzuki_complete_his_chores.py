8886b227000ac6c23a57c79c3a6da9909db2ecc5
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 11:06:08 +0300

    Help Suzuki complete his chores!

---
+++ 7-kyu/help_suzuki_complete_his_chores.py
@@ -0,0 +1,15 @@
+def chore_assignment(chores):
+    chores_sorted = sorted(chores)
+    workloads = []
+    left = 0
+    right = len(chores_sorted) - 1
+    
+    while left <= right:
+        if left == right:
+            workloads.append(chores_sorted[left])
+        else:
+            workloads.append(chores_sorted[left] + chores_sorted[right])
+        left += 1
+        right -= 1
+        
+    return sorted(workloads)
