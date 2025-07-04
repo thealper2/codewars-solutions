656aa74e2aec7d26d6d701efd0e92f97e95cd1d9
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 13:36:15 +0300

    Format to the 2nd

---
+++ 7-kyu/format_to_the_2nd.py
@@ -0,0 +1,20 @@
+def print_nums(*args):
+    numbers = list(args)
+    if not numbers:
+        return ''
+    
+    if len(numbers) == 1:
+        return str(numbers[0])
+    
+    max_num = max(numbers)
+    max_length = len(str(max_num))
+    formatted_numbers = []
+    for num in numbers:
+        num_str = str(num)
+        if num == max_num:
+            formatted_numbers.append(num_str)
+        else:
+            formatted_num = num_str.zfill(max_length)
+            formatted_numbers.append(formatted_num)
+    
+    return '\n'.join(formatted_numbers)
