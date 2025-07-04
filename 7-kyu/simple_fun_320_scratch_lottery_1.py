daf34c0242698a31fca8dbd62e0790e7d092d701
Alper Karaca <thisiseternity12@gmail.com>
2025-07-04 22:19:49 +0300

    Simple Fun #320: Scratch lottery 1

---
+++ 7-kyu/simple_fun_320_scratch_lottery_1.py
@@ -0,0 +1,16 @@
+def scratch(lottery):
+    result = 0
+    for x in lottery:
+        words = x.split()
+        reward = int(words[-1])
+        temp = words[0]
+        found = True
+        for word in words[1:-1]:
+            if word != temp:
+                found = False
+                break
+                
+        if found:
+            result += reward
+        
+    return result
