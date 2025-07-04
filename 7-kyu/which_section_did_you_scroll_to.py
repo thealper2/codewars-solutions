8d3af0e62a17588cc39d1980523eb0057e96bd5b
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 17:04:18 +0300

    Which section did you scroll to?

---
+++ 7-kyu/which_section_did_you_scroll_to.py
@@ -0,0 +1,9 @@
+def get_section_id(scroll, sizes):
+    cumulative_size = 0
+    for i, size in enumerate(sizes):
+        if scroll < cumulative_size + size:
+            return i
+        
+        cumulative_size += size
+    
+    return -1
