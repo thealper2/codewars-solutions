46a8c4ada84420bbc3055ec6804d82917b71ccdb
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 11:34:14 +0300

    Email Address Obfuscator

---
+++ 7-kyu/email_address_obfuscator.py
@@ -0,0 +1,15 @@
+def obfuscate(email):
+    result = ""
+    temp = ""
+    for c in email:
+        if c == ".":
+            result += temp + " [dot] "
+            temp = ""
+        elif c == "@":
+            result += temp + " [at] "
+            temp = ""
+        else:
+            temp += c
+            
+    result += temp
+    return result
