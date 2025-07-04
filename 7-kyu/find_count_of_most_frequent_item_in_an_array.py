e21338e673b6a80970dc50686dbf45fbc9eb4c7d
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 11:20:18 +0300

    Find Count of Most Frequent Item in an Array

---
+++ 7-kyu/find_count_of_most_frequent_item_in_an_array.py
@@ -0,0 +1,17 @@
+def most_frequent_item_count(collection):
+    if not collection:
+        return 0
+    
+    count = {}
+    max_val = float('-inf')
+    
+    for i in collection:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
+            
+        if count[i] > max_val:
+            max_val = count[i]
+            
+    return max_val
