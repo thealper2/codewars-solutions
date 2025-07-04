dbfa41970ed863c54f5397a1584c91016201b4ed
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 10:44:44 +0300

    Katastrophe!

---
+++ 7-kyu/katastrophe.py
@@ -0,0 +1,14 @@
+def strong_enough(earthquake, age):
+    total = 1
+    strength = 1000
+
+    for q in earthquake:
+        total *= sum(q)
+    
+    for year in range(1, age + 1):
+        strength = strength - (strength * 0.01)
+        
+    if total < strength:
+        return "Safe!"
+    
+    return "Needs Reinforcement!"
