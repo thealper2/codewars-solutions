445ec7271112a26e287e1dbf1dde5e5de0b20a74
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 15:37:28 +0300

    Is every value in the array an array?

---
+++ 7-kyu/is_every_value_in_the_array_an_array.py
@@ -0,0 +1,8 @@
+def arr_check(arr):
+    if not isinstance(arr, list):
+        return False
+    for element in arr:
+        if not isinstance(element, list):
+            return False
+        
+    return True
