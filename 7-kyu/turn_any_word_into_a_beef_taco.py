5ea19f9a51182724e3d33031121c36f65e9e01f4
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 11:47:28 +0300

    Turn any word into a beef taco

---
+++ 7-kyu/turn_any_word_into_a_beef_taco.py
@@ -0,0 +1,19 @@
+def tacofy(word):
+    taco_ingredients = ['shell']
+    lower_word = word.lower()
+    for char in lower_word:
+        if char in {'a', 'e', 'i', 'o', 'u'}:
+            taco_ingredients.append("beef")
+        elif char == 't':
+            taco_ingredients.append("tomato")
+        elif char == 'l':
+            taco_ingredients.append("lettuce")
+        elif char == 'c':
+            taco_ingredients.append("cheese")
+        elif char == 'g':
+            taco_ingredients.append("guacamole")
+        elif char == 's':
+            taco_ingredients.append("salsa")
+            
+    taco_ingredients.append('shell')
+    return taco_ingredients
