d5e77a12ba66de89270b869c41a47f58f04e7bdb
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-04 16:49:21 +0300

    Status Arrays

---
+++ 7-kyu/status_arrays.py
@@ -0,0 +1,10 @@
+def status(nums):
+    elements_with_status = []
+    for index, num in enumerate(nums):
+        less_count = sum(1 for x in nums if x < num)
+        status = index + less_count
+        elements_with_status.append((status, index, num))
+    
+    elements_with_status.sort(key=lambda x: (x[0], x[1]))
+    result = [num for status, index, num in elements_with_status]
+    return result
