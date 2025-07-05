b3797596d83008422833a9279b1d97ed09ce9f71
Alper Karaca <thisiseternity12@gmail.com>
2025-07-05 22:52:43 +0300

    Nice Array

---
+++ 7-kyu/nice_array.py
@@ -0,0 +1,10 @@
+def is_nice(arr):
+    if not arr:
+        return False
+    
+    arr = set(arr)
+    for c in arr:
+        if (c - 1 not in arr) and (c + 1 not in arr):
+            return False
+        
+    return True
