d3d0e1450293e98996b46d89a6e8b317872d6ae5
Alper Karaca <thisiseternity12@gmail.com>
2025-07-05 23:06:05 +0300

    How many urinals are free?

---
+++ 7-kyu/how_many_urinals_are_free.py
@@ -0,0 +1,16 @@
+def get_free_urinals(urinals):
+    urinals = list(urinals)
+    x = len(urinals)
+    count = 0
+    
+    for i in range(x - 1):
+        if urinals[i] == '1' and urinals[i + 1] == '1':
+            return -1
+    
+    for i in range(x):
+        if urinals[i] == '0':
+            if ((i == 0) or (urinals[i - 1] == '0')) and ((i == x - 1) or (urinals[i + 1] == '0')):
+                urinals[i] = '1'
+                count += 1
+
+    return count
