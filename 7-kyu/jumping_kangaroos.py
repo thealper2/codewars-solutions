d61bcdbed2ae2eaef65f0a14251c4ced223b8326
Alper Karaca <thisiseternity12@gmail.com>
2025-07-05 21:56:06 +0300

    Jumping Kangaroos

---
+++ 7-kyu/jumping_kangaroos.py
@@ -0,0 +1,6 @@
+def kangaroo(kanga1, rate1, kanga2, rate2):
+    if rate1 == rate2:
+        return kanga1 == kanga2
+    
+    t = (kanga2 - kanga1) / (rate1 - rate2)
+    return t >= 0 and t.is_integer()
