5fa8d68983fe9a8ed062cfc14601d1df8b9e8c78
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 00:08:07 +0300

    Regexp Basics - is it a eight bit signed number?

---
+++ 7-kyu/regexp_basics_is_it_a_eight_bit_signed_number.py
@@ -0,0 +1,6 @@
+import re
+
+
+def signed_eight_bit_number(number):
+    pattern = r'^(0|[1-9][0-9]?|1[01][0-9]|12[0-7]|-([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8]))$'
+    return bool(re.fullmatch(pattern, number))
