400c36dedfe2cfd0074143daa09b259b86fe87b8
Alper Karaca <thisiseternity12@gmail.com>
2025-07-05 22:47:12 +0300

    Polydivisible Numbers

---
+++ 7-kyu/polydivisible_numbers.py
@@ -0,0 +1,8 @@
+def polydivisible(x):
+    x = str(x)
+    for i in range(1, len(x) + 1):
+        sub_num = int(x[:i])
+        if sub_num % i != 0:
+            return False
+        
+    return True
