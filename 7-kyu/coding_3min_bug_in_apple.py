ba5877c9b71e287fe8917ea26c19452f3cb504b4
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 14:36:37 +0300

    Coding 3min: Bug in Apple

---
+++ 7-kyu/coding_3min_bug_in_apple.py
@@ -0,0 +1,7 @@
+def sc(apple):
+    for i, row in enumerate(apple):
+        for j, cell in enumerate(row):
+            if cell == 'B':
+                return (i, j)
+            
+    return (-1, -1)
