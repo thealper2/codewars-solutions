c24ddb47d06da67f761d27629051cfc38b187d95
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 17:40:01 +0300

    How Green Is My Valley?

---
+++ 7-kyu/how_green_is_my_valley.py
@@ -0,0 +1,26 @@
+def make_valley(arr):
+    if not arr:
+        return []
+    
+    arr.sort()
+    result = [arr[0]]
+    n = len(arr)
+    l = False
+    if n % 2 == 0:
+        for i in range(1, n):
+            if l:
+                result.append(arr[i])
+            else:
+                result.insert(0, arr[i])
+    
+            l = not l
+
+    else:
+        for i in range(1, n):
+            if l:
+                result.insert(0, arr[i])
+            else:
+                result.append(arr[i])
+
+            l = not l
+    return result
