a0ce740fa67af71bad6ada122d76d6a7686921cf
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 17:06:28 +0300

    Geometry Basics: Distance between circles in 2D

---
+++ 7-kyu/geometry_basics_distance_between_circles_in_2d.py
@@ -0,0 +1,12 @@
+import math
+
+
+def distance_between_circles(a, b):
+    dx = a.center.x - b.center.x
+    dy = a.center.y - b.center.y
+    distance = math.sqrt(dx ** 2 + dy ** 2)
+    sum_radii = a.radius + b.radius
+    if distance <= sum_radii:
+        return 0
+    
+    return distance - sum_radii
