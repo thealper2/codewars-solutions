7a4c6cdf46b3a60f01b1302eef8035c3868b6837
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 14:47:05 +0300

    Calculate Two People's Individual Ages

---
+++ 7-kyu/calculate_two_peoples_individual_ages.py
@@ -0,0 +1,15 @@
+def get_ages(sum_, difference):
+    y = (sum_ + difference) / 2
+    x = sum_ - y
+    
+    if x < 0 or y < 0:
+        return None
+    
+    if abs(x + y) != sum_ or abs(x - y) != difference:
+        return None
+
+    if x > y:
+        return (x, y)
+    
+    return (y, x)
+    
