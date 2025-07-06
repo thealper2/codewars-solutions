239ad83c6ce2ccf832fa9f33af9a871955d9d6e5
Alper Karaca <thisiseternity12@gmail.com>
2025-07-06 17:44:29 +0300

    Possibilities Array

---
+++ 7-kyu/possibilities_array.py
@@ -0,0 +1,11 @@
+def is_all_possibilities(arr):
+    n = len(arr)
+    if n == 0:
+        return False
+    
+    arr.sort()
+    for i in range(n):
+        if arr[i] != i:
+            return False
+    
+    return True
