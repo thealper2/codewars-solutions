a1d4cbdf69c1425e70cd48089cfb50f4a0ee8aff
Alper Karaca <thisiseternity12@gmail.com>
2025-07-08 23:06:13 +0300

    String doubles

---
+++ 7-kyu/string_doubles.py
@@ -0,0 +1,19 @@
+def doubles(s):
+    changed = True
+    
+    while changed:
+        changed = False
+        i = 0
+        new_s = ""
+        n = len(s)
+    
+        while i < n:
+                if i + 1 < n and s[i] == s[i + 1]:
+                    i += 2
+                    changed = True
+                else:
+                    new_s += s[i]
+                    i += 1
+
+        s = new_s
+    return s
