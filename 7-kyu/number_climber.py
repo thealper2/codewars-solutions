5898e04db7f1ed653963adc7d03e65a5d322b265
Alper Karaca <thisiseternity12@gmail.com>
2025-07-09 23:40:01 +0300

    Number climber

---
+++ 7-kyu/number_climber.py
@@ -0,0 +1,10 @@
+def climb(n):
+    sequence = []
+    while n >= 1:
+        sequence.insert(0, n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (n - 1) // 2
+    
+    return sequence
