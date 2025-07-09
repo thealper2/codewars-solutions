e00e3107fd5df0483cde1075b5347e29d4ee0cf0
Alper Karaca <thisiseternity12@gmail.com>
2025-07-09 23:07:48 +0300

    Scrolling Text

---
+++ 7-kyu/scrolling_text.py
@@ -0,0 +1,9 @@
+def scrolling_text(text):
+    n = len(text)
+    text = text.upper()
+    result = []
+    
+    for i in range(n):
+        result.append(text[i:] + text[:i])
+        
+    return result
