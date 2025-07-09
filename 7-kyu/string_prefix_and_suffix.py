3d14736a0a519d815ae4735738c6afc045d4e98f
Alper Karaca <thisiseternity12@gmail.com>
2025-07-09 20:17:29 +0300

    String prefix and suffix

---
+++ 7-kyu/string_prefix_and_suffix.py
@@ -0,0 +1,10 @@
+def solve(st):
+    n = min(len(st) // 2, len(st) - 1)
+    max_len = 0
+    
+    for l in range(n, 0, -1):
+        if st[:l] == st[-l:]:
+            max_len = l
+            break
+            
+    return max_len
