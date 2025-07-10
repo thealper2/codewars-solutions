06bd6997c52cc40c49b74f059bcb6ce20373876c
Alper Karaca <thisiseternity12@gmail.com>
2025-07-10 22:57:10 +0300

    Lorraine Wants to Win the TV Contest

---
+++ 7-kyu/lorraine_wants_to_win_the_tv_contest.py
@@ -0,0 +1,18 @@
+from typing import defaultdict
+
+
+def unscramble(scramble):
+    global word_list
+    scrambled_count = defaultdict(int)
+    for letter in scramble.lower():
+        scrambled_count[letter] += 1
+    
+    valid_words = []
+    for word in word_list:
+        word_count = defaultdict(int)
+        for letter in word.lower():
+            word_count[letter] += 1
+        if word_count == scrambled_count:
+            valid_words.append(word)
+            
+    return valid_words
