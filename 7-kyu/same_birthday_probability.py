4965e52a05e095019d1f96a00c76432ce206cb09
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 13:45:45 +0300

    Same Birthday Probability

---
+++ 7-kyu/same_birthday_probability.py
@@ -0,0 +1,10 @@
+def calculate_probability(n):
+    if n < 2:
+        return 0.0
+    
+    probability = 1.0
+    for i in range(n):
+        probability *= (365 - i) / 365
+    
+    result = 1 - probability
+    return round(result, 2) if result not in (0, 1) else int(result)
