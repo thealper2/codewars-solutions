a248eb4ab0e8d5030d31562e998d9bb0c8c4bc47
Alper Karaca <thisiseternity12@gmail.com>
2025-07-10 23:04:59 +0300

    The Office 2 - Boredom Score

---
+++ 7-kyu/the_office_2_boredom_score.py
@@ -0,0 +1,24 @@
+def boredom(staff):
+    department_scores = {
+        'accounts': 1,
+        'finance': 2,
+        'canteen': 10,
+        'regulation': 3,
+        'trading': 6,
+        'change': 6,
+        'IS': 8,
+        'retail': 5,
+        'cleaning': 4,
+        'pissing about': 25
+    }
+    
+    total_score = 0
+    for department in staff.values():
+        total_score += department_scores.get(department, 0)
+    
+    if total_score <= 80:
+        return 'kill me now'
+    elif total_score < 100:
+        return 'i can handle this'
+    else:
+        return 'party time!!'
