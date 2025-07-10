624122bc2a16c476f78a97661fb630b7dc6cac56
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 09:25:56 +0300

    Unix command line 'ls -l' extract the file type.

---
+++ 7-kyu/unix_command_line_ls_l_extract_the_file_type.py
@@ -0,0 +1,13 @@
+def linux_type(file_attribute):
+    types = {
+        "-": "file",
+        "d": "directory",
+        "l": "symlink",
+        "c": "character_file",
+        "b": "block_file",
+        "p": "pipe",
+        "s": "socket",
+        "D": "door"
+    }
+    
+    return types[file_attribute[0]]
