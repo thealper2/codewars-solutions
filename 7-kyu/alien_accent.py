98bfc1b174e807c5f8680ad62add3bd3ee953566
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 11:20:14 +0300

    Alien Accent

---
+++ 7-kyu/alien_accent.py
@@ -0,0 +1,11 @@
+def convert(st):
+    result = ""
+    for c in st:
+        if c == 'o':
+            result += 'u'
+        elif c == 'a':
+            result += 'o'
+        else:
+            result += c
+            
+    return result
