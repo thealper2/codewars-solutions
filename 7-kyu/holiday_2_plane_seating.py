26e26904c97f4a3a509ee14fe7977445c973780e
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 10:30:53 +0300

    Holiday 2 - Plane Seating

---
+++ 7-kyu/holiday_2_plane_seating.py
@@ -0,0 +1,22 @@
+def plane_seat(a):
+    p1 = p2 = ""
+    
+    if int(a[:-1]) <= 20:
+        p1 = "Front"
+    elif int(a[:-1]) <= 40:
+        p1 = "Middle"
+    elif int(a[:-1]) <= 60:
+        p1 = "Back"
+    else:
+        return "No Seat!!"
+        
+    if ord(a[-1]) <= 67:
+        p2 = "Left"
+    elif ord(a[-1]) <= 70:
+        p2 = "Middle"
+    elif ord(a[-1]) <= 72 or ord(a[-1]) == 75:
+        p2 = "Right"
+    else:
+        return "No Seat!!"
+    
+    return p1 + '-' + p2
