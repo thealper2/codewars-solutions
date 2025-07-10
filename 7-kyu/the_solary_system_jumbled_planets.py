327b07ae9ae86baded2221211e93d9495ee54fa4
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 14:13:49 +0300

    The Solar System - Jumbled Planets

---
+++ 7-kyu/the_solary_system_jumbled_planets.py
@@ -0,0 +1,32 @@
+def jumbled_solar_system(solar_system):
+    if not solar_system:
+        return []
+    
+    size_order = {
+        'Asteroid': 0,
+        'Pluto': 1,
+        'Mercury': 2,
+        'Mars': 3,
+        'Venus': 4,
+        'Earth': 5,
+        'Neptune': 6,
+        'Uranus': 7,
+        'Saturn': 8,
+        'Jupiter': 9
+    }
+    
+    result = []
+    for i in range(1, len(solar_system)):
+        current = solar_system[i]
+        previous = solar_system[i-1]
+        
+        if current == "Asteroid" and previous == "Asteroid":
+            result.append('=')
+        elif size_order[current] > size_order[previous]:
+            result.append('>')
+        elif size_order[current] < size_order[previous]:
+            result.append('<')
+        else:
+            result.append('=')
+    
+    return result
