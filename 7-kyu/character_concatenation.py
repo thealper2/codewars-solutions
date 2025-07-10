70256799eb3821f88f5415e7f7ab5bc2481ae473
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 14:18:47 +0300

    Character Concatenation

---
+++ 7-kyu/character_concatenation.py
@@ -0,0 +1,9 @@
+def char_concat(word):
+    result = []
+    n = len(word)
+    for i in range(n // 2):
+        left_char = word[i]
+        right_char = word[n - 1 - i]
+        result.append(f"{left_char}{right_char}{i+1}")
+        
+    return ''.join(result)
