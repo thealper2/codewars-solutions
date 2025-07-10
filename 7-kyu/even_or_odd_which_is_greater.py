8a40d054dc71bb97df80c3bf933807dd529a60b5
Alper Karaca <thisiseternity12@gmail.com>
2025-07-10 23:39:24 +0300

    Even or Odd - Which is Greater?

---
+++ 7-kyu/even_or_odd_which_is_greater.py
@@ -0,0 +1,18 @@
+def even_or_odd(s):
+    sum_even = 0
+    sum_odd = 0
+    
+    for c in s:
+        if int(c) % 2 == 0:
+            sum_even += int(c)
+        else:
+            sum_odd += int(c)
+            
+    if sum_odd > sum_even:
+        return "Odd is greater than Even"
+    
+    elif sum_even > sum_odd:
+        return "Even is greater than Odd"
+    
+    else:
+        return "Even and Odd are the same"
