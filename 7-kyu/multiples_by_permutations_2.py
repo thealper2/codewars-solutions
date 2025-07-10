b4595ec66b71d67c7a2d6bf3881d1381e545f30a
Alper Karaca <thisiseternity12@gmail.com>
2025-07-10 23:03:38 +0300

    Multiples By Permutations 2

---
+++ 7-kyu/multiples_by_permutations_2.py
@@ -0,0 +1,11 @@
+def find_lowest_int(k):
+    k2 = k + 1
+    n = 1
+    while True:
+        prod1 = k * n
+        prod2 = k2 * n
+        
+        if sorted(str(prod1)) == sorted(str(prod2)):
+            return n
+        
+        n += 1
