10c3c2c67f9e46b46c38e07f326c446005bc9495
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 09:29:41 +0300

    Unflatten a list (Easy)

---
+++ 7-kyu/unflatten_a_list_easy.py
@@ -0,0 +1,20 @@
+def unflatten(flat_array):
+    result = []
+    i = 0
+    n = len(flat_array)
+    
+    while i < n:
+        current = flat_array[i]
+        if current < 3:
+            result.append(current)
+            i += 1
+        else:
+            sub_array_length = current
+            end = i + sub_array_length
+            if end > n:
+                end = n
+            sub_array = flat_array[i:end]
+            result.append(sub_array)
+            i = end
+
+    return result
