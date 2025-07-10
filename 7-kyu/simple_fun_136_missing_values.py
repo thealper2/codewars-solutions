cd311d108bcebe43edf1a057101d83f893322afd
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 15:55:31 +0300

    Simple Fun #136: Missing Values

---
+++ 7-kyu/simple_fun_136_missing_values.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+def missing_values(seq): 
+    freq = defaultdict(int)
+    for num in seq:
+        freq[num] += 1
+    
+    x = None
+    y = None
+    for num, count in freq.items():
+        if count == 1:
+            x = num
+        elif count == 2:
+            y = num
+    
+    return x * x * y
