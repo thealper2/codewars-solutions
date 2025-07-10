e7fe62c25edefb58435f7308c17a32df2e6764bb
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 15:57:30 +0300

    C is for Codewars

---
+++ 7-kyu/c_is_for_codewars.py
@@ -0,0 +1,11 @@
+def generate_C(size):
+    top_bottom_line = 'C' * (5 * size)
+    middle_line = 'C' * size
+    
+    top_section = [top_bottom_line for _ in range(size)]
+    middle_section = [middle_line for _ in range(3 * size)]
+    bottom_section = [top_bottom_line for _ in range(size)]
+    
+    all_lines = top_section + middle_section + bottom_section
+    result = '\n'.join(all_lines)
+    return result
