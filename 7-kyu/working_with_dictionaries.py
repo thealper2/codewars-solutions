69c32bd84bd823019120336e678ff5ca1764ed2f
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 17:09:49 +0300

    Working with Dictionaries

---
+++ 7-kyu/working_with_dictionaries.py
@@ -0,0 +1,18 @@
+from preloaded import A001055
+
+def inf_database(range_, str_, val):
+    tmp = []
+    if str_ == "equals to":
+        tmp = [i for i in range(range_[0], range_[1]+1) if A001055[i] == val]
+    elif str_ == "higher than":
+        tmp = [i for i in range(range_[0], range_[1]+1) if A001055[i] > val]
+    elif str_ == "lower than":
+        tmp = [i for i in range(range_[0], range_[1]+1) if A001055[i] < val]
+    elif str_ == "higher and equals to":
+        tmp = [i for i in range(range_[0], range_[1]+1) if A001055[i] >= val]
+    elif str_ == "lower and equals to":
+        tmp = [i for i in range(range_[0], range_[1]+1) if A001055[i] <= val]
+    else:
+        return 'wrong constraint'
+    
+    return (len(tmp), tmp)
