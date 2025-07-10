222f33dd5868fafbcaf534e85230a82d949c3578
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 13:37:05 +0300

    Find the vowels

---
+++ 7-kyu/find_the_vowels.py
@@ -0,0 +1,11 @@
+def vowel_indices(word):
+    n = len(word)
+    vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+    result = []
+    
+    for i in range(n):
+        if word[i].lower() in vowels:
+            result.append(i + 1)
+            
+    return result
+        
