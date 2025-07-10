ced81a1dcbeaf2295dc47bb68814cf56d4c69db8
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 14:16:06 +0300

    Splitting Numbers

---
+++ 7-kyu/splitting_numbers.py
@@ -0,0 +1,17 @@
+def split_numbers(n: int) -> tuple[int, int]:
+    a = 0
+    b = 0
+    bit_pos = 0
+    count = 0
+    
+    while n > 0:
+        if n & 1:
+            if count % 2 == 0:
+                a |= (1 << bit_pos)
+            else:
+                b |= (1 << bit_pos)
+            count += 1
+        n >>= 1
+        bit_pos += 1
+    
+    return (a, b)
