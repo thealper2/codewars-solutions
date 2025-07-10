f1da186621f9c19ff6428578ba3cbb6ae98a6e2d
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 15:19:21 +0300

    Simple Fun #62: Draw Rectangle

---
+++ 7-kyu/simple_fun_62_draw_rectangle.py
@@ -0,0 +1,13 @@
+def draw_rectangle(canvas, rectangle):
+    x1, y1, x2, y2 = rectangle
+    for y in range(y1, y2 + 1):
+        for x in range(x1, x2 + 1):
+            if y == y1 or y == y2:
+                if x == x1 or x == x2:
+                    canvas[y][x] = '*'
+                else:
+                    canvas[y][x] = '-'
+            else:
+                if x == x1 or x == x2:
+                    canvas[y][x] = '|'
+    return canvas
