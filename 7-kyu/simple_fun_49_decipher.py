1b7daf71a4c9577981d233b083b22b8d01078bf1
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 15:33:55 +0300

    Simple Fun #49: Decipher

---
+++ 7-kyu/simple_fun_49_decipher.py
@@ -0,0 +1,14 @@
+def decipher(cipher):
+    plaintext = ""
+    i = 0
+    n = len(cipher)
+    
+    while i < n:
+        if int(cipher[i:i+2]) < 97:
+            plaintext += chr(int(cipher[i:i+3]))
+            i += 3
+        else:
+            plaintext += chr(int(cipher[i:i+2]))
+            i += 2
+            
+    return plaintext
