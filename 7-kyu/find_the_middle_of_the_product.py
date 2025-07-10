7fb79eb75d79d35019d23f9fefd36da16eef9787
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 16:48:35 +0300

    Find the Middle of the Product

---
+++ 7-kyu/find_the_middle_of_the_product.py
@@ -0,0 +1,33 @@
+def find_middle(s):
+    if not isinstance(s, str):
+        return -1
+    
+    digits = []
+    for char in s:
+        if char.isdigit():
+            digits.append(int(char))
+    
+    if not digits:
+        return -1
+    
+    product = 1
+    for num in digits:
+        product *= num
+    
+    product_str = str(product)
+    length = len(product_str)
+    
+    if length == 0:
+        return -1
+    
+    if length % 2 == 1:
+        middle_index = length // 2
+        return int(product_str[middle_index])
+    else:
+        middle_start = (length // 2) - 1
+        middle_two = product_str[middle_start:middle_start + 2]
+        if middle_two[0] == '0':
+            middle_two = middle_two.lstrip('0')
+            if not middle_two:
+                return 0
+        return int(middle_two)
