cd4268faba4b7e86d5ec8b957810c423909daecc
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 10:55:11 +0300

    Inspiring Strings

---
+++ 7-kyu/inspiring_strings.py
@@ -0,0 +1,10 @@
+def longest_word(string_of_words):
+    words = string_of_words.split()
+    n = len(words)
+    longest_word = ""
+    
+    for word in words:
+        if len(longest_word) <= len(word):
+            longest_word = word
+            
+    return longest_word
