55d13fde720d2baf698d5be46b1ebaa0d56cab4a
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 17:11:39 +0300

    Vowel one

---
+++ 7-kyu/vowel_one.py
@@ -0,0 +1,9 @@
+def vowel_one(s):
+    vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    result = []
+    for char in s:
+        if char in vowels:
+            result.append('1')
+        else:
+            result.append('0')
+    return ''.join(result)
