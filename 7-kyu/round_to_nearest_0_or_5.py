ca448250b3c3dba1fcbbd71d51241cfe9dd81420
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-10 09:39:17 +0300

    Round to nearest 0 or 5

---
+++ 7-kyu/round_to_nearest_0_or_5.py
@@ -0,0 +1,9 @@
+def round_to_five(arr):
+    rounded_numbers = []
+    for num in arr:
+        quotient = num / 5
+        rounded_quotient = int(quotient + 0.5) if quotient % 1 >= 0.5 else int(quotient)
+        rounded_num = rounded_quotient * 5
+        rounded_numbers.append(rounded_num)
+        
+    return rounded_numbers
