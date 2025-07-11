967875f4bae6f00670d74c100a2c7855939c4ea3
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 10:14:02 +0300

    Simple Fun #203: Strange Coach

---
+++ 7-kyu/simple_fun_203_strange_coach.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+def strange_coach(players):
+    letter_counts = defaultdict(int)
+    for surname in players:
+        if surname:
+            first_letter = surname[0].lower()
+            letter_counts[first_letter] += 1
+
+    possible_letters = [letter for letter, count in letter_counts.items() if count >= 5]
+    possible_letters.sort()
+
+    if not possible_letters:
+        return "forfeit"
+    else:
+        return ''.join(possible_letters)
