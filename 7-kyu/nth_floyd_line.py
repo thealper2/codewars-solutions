da40c84cb22e53761c442c9e62f427870ff8426e
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 14:12:57 +0300

    nth Floyd line

---
+++ 7-kyu/nth_floyd_line.py
@@ -0,0 +1,9 @@
+def nth_floyd(n):
+    i = 1
+    while True:
+        lb = (i * (i - 1)) // 2 + 1
+        ub = (i * (i + 1)) // 2
+        if lb <= n <= ub:
+            return i
+        
+        i += 1
