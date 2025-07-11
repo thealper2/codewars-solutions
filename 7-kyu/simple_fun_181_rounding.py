a82cf7298a2f73e48cd4d05c4a0e9425eb0b5188
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 10:52:42 +0300

    Simple Fun #181: Rounding

---
+++ 7-kyu/simple_fun_181_rounding.py
@@ -0,0 +1,13 @@
+def rounding(n,m):
+    lower = (n // m) * m
+    upper = lower + m
+    
+    diff_lower = n - lower
+    diff_upper = upper - n
+    
+    if diff_lower < diff_upper:
+        return lower
+    elif diff_upper < diff_lower:
+        return upper
+    else:
+        return n
