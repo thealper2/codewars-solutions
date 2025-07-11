941eb47d69c28581bc7eb55fd42dd44f2c42c258
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 10:43:01 +0300

    Simple Fun #18: Candles

---
+++ 7-kyu/simple_fun_18_candles.py
@@ -0,0 +1,12 @@
+def candles(m, n):
+    total = 0
+    leftovers = 0
+    current = m
+    
+    while current > 0:
+        total += current
+        leftovers += current
+        current = leftovers // n
+        leftovers %= n
+    
+    return total
