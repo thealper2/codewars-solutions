9110d6b02060a5ec507aa8df2a82e93713ed57dd
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 11:33:16 +0300

    Simple Fun #157: Counting Triangles

---
+++ 7-kyu/simple_fun_157_counting_triangles.py
@@ -0,0 +1,14 @@
+def counting_triangles(V):
+    n = len(V)
+    count = 0
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                a = V[i]
+                b = V[j]
+                c = V[k]
+                if a + b > c and a + c > b and b + c > a:
+                    count += 1
+                        
+    return count
