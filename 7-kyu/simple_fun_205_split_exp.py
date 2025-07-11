20b98454d7c7c4c3ca7d1f9dc0e219ae8aa9b504
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 10:06:38 +0300

    Simple Fun #205: Split Exp

---
+++ 7-kyu/simple_fun_205_split_exp.py
@@ -0,0 +1,23 @@
+def split_exp(n):
+    if n == "0":
+        return []
+    
+    result = []
+    if '.' in n:
+        integer_part, fractional_part = n.split('.')
+    else:
+        integer_part = n
+        fractional_part = ''
+    
+    for i, digit in enumerate(integer_part):
+        if digit != '0':
+            power = len(integer_part) - i - 1
+            num = digit + '0' * power
+            result.append(num)
+    
+    for j, digit in enumerate(fractional_part):
+        if digit != '0':
+            num = '.' + '0' * j + digit
+            result.append(num)
+    
+    return sorted(result, key=lambda x: float(x), reverse=True)
