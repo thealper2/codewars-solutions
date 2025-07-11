293312768393da834f0a76193ed0c3392e29e258
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 16:43:22 +0300

    Thinking & Testing: Sport Star

---
+++ 7-kyu/thinking_and_testing_sport_star.py
@@ -0,0 +1,19 @@
+def testit(actions, hurdles):
+    hurdles = list(hurdles)
+    hurdle_index = 0
+    for action in actions:
+        if hurdle_index >= len(hurdles):
+            hurdle_index = 0
+        current_hurdle = hurdles[hurdle_index]
+        if action == "run":
+            if current_hurdle == "|":
+                hurdles[hurdle_index] = "/"
+            elif current_hurdle == "x":
+                hurdles[hurdle_index] = "/"
+        elif action == "jump":
+            if current_hurdle == "_":
+                hurdles[hurdle_index] = "x"
+            elif current_hurdle == "/":
+                hurdles[hurdle_index] = "x"
+        hurdle_index += 1
+    return ''.join(hurdles)
