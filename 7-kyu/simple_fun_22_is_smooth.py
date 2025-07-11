b086e132e4b740fd7ca0075f0fa122bbfc418375
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 09:55:30 +0300

    Simple Fun #22: Is Smooth?

---
+++ 7-kyu/simple_fun_22_is_smooth.py
@@ -0,0 +1,14 @@
+def is_smooth(arr):
+    n = len(arr)
+    if n == 0:
+        return False
+    
+    if arr[0] != arr[-1]:
+        return False
+    
+    if n % 2 == 1:
+        middle = arr[n//2]
+    else:
+        middle = arr[n//2 - 1] + arr[n//2]
+    
+    return arr[0] == middle
