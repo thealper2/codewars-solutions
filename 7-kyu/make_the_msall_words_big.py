59de9867e6e34e67b7c3ad178162ef5510b3ace1
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 16:16:57 +0300

    Make the small words big!

---
+++ 7-kyu/make_the_msall_words_big.py
@@ -0,0 +1,17 @@
+def small_word_helper(sentence):
+    words = sentence.split()
+    result = []
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    for word in words:
+        n = len(word)
+        if n <= 3:
+            result.append(word.upper())
+        else:
+            temp = ""
+            for c in word:
+                if c.lower() not in vowels:
+                    temp += c
+                    
+            result.append(temp)
+            
+    return ' '.join(result)
