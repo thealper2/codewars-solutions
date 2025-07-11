245fe42f49cb1462e1cbfaf7cceb5da04cf2fbe8
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 09:44:03 +0300

    Simple Fun #35: Different Squraes

---
+++ 7-kyu/simple_fun_35_different_squares.py
@@ -0,0 +1,9 @@
+def different_squares(matrix):
+    squares = set()
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows > 0 else 0
+    for i in range(rows - 1):
+        for j in range(cols - 1):
+            square = (matrix[i][j], matrix[i][j+1], matrix[i+1][j], matrix[i+1][j+1])
+            squares.add(square)
+    return len(squares)
