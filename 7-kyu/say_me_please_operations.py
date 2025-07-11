0c0443db2573abda02494bbba08b21c3d53f1a55
Alper Karaca <thisiseternity12@gmail.com>
2025-07-12 00:01:16 +0300

    Say Me Please Operations

---
+++ 7-kyu/say_me_please_operations.py
@@ -0,0 +1,22 @@
+def say_me_operations(string_numbers):
+    numbers = list(map(int, string_numbers.split()))
+    operations = []
+    
+    for i in range(2, len(numbers)):
+        first = numbers[i - 2]
+        second = numbers[i - 1]
+        current = numbers[i]
+        
+        if first + second == current:
+            operations.append("addition")
+        elif first - second == current:
+            operations.append("subtraction")
+        elif first * second == current:
+            operations.append("multiplication")
+        elif second != 0 and first // second == current:
+            operations.append("division")
+        else:
+            operations.append("unknown")
+    
+    return ", ".join(operations)
+        
