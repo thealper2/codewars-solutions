1290b32929a890cba90635e65c609799c1695fb9
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 16:56:52 +0300

    Strong Number (Special Numbers Series #2)

---
+++ 7-kyu/strong_number_special_numbers_series_2.py
@@ -0,0 +1,14 @@
+def strong_num(number):
+    factorials = [0] * 10
+    factorials[0] = factorials[1] = 1;
+    for i in range(2,10) :
+        factorials[i] = factorials[i-1] * i
+    
+    fact_sum = 0
+    temp = number
+
+    while temp:
+        fact_sum = fact_sum + factorials[temp % 10]
+        temp = temp // 10
+
+    return "STRONG!!!!" if fact_sum == number else "Not Strong !!"
