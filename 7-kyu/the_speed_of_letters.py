5c0c897938d5b7594a9c94a94ce246cf9479022c
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-11 16:48:21 +0300

    The Speed of Letters

---
+++ 7-kyu/the_speed_of_letters.py
@@ -0,0 +1,16 @@
+def speedify(st): 
+    if not st:
+        return ""
+    
+    max_shift = 25
+    max_length = len(st) + max_shift
+    output = [' '] * max_length
+    
+    for idx, char in enumerate(st):
+        shift = ord(char) - ord('A')
+        new_pos = idx + shift
+        if new_pos < max_length:
+            output[new_pos] = char
+    
+    result = ''.join(output).rstrip()
+    return result
