0ac02cd9e5c0d37a97fe2bd0a0f4886a14bc4522
Alper Karaca <thisiseternity12@gmail.com>
2025-07-12 16:48:53 +0300

    number with 3 roots.

---
+++ 7-kyu/number_with_3_roots.py
@@ -0,0 +1,17 @@
+import math
+
+
+def perfect_roots(n):
+    sqrt_2nd = math.isqrt(n)
+    if sqrt_2nd * sqrt_2nd != n:
+        return False
+    
+    sqrt_4th = math.isqrt(sqrt_2nd)
+    if sqrt_4th * sqrt_4th != sqrt_2nd:
+        return False
+    
+    sqrt_8th = math.isqrt(sqrt_4th)
+    if sqrt_8th * sqrt_8th != sqrt_4th:
+        return False
+    
+    return True
