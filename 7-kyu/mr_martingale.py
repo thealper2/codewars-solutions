0d4864ab044308b069199e7d3ac256d060078d86
Alper Karaca <thisiseternity12@gmail.com>
2025-07-12 23:45:01 +0300

    Mr Martingale

---
+++ 7-kyu/mr_martingale.py
@@ -0,0 +1,14 @@
+def martingale(bank, outcomes):
+    if not outcomes:
+        return bank
+    
+    stake = 100
+    for outcome in outcomes:
+        if outcome == 0:
+            bank -= stake
+            stake *= 2
+        elif outcome == 1:
+            bank += stake
+            stake = 100
+        
+    return bank
