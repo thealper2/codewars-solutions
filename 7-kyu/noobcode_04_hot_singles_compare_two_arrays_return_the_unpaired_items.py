8edfe175180a8f3fcc3fdfa3df92350f7b0cff6f
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 22:33:52 +0300

    noobCode 04: HOT SINGLES...compare the arrays, return the unpaired items !

---
+++ 7-kyu/noobcode_04_hot_singles_compare_two_arrays_return_the_unpaired_items.py
@@ -0,0 +1,11 @@
+def hot_singles(arr1, arr2):
+    result = []
+    for item in arr1:
+        if item not in arr2 and item not in result:
+            result.append(item)
+        
+    for item in arr2:
+        if item not in arr1 and item not in result:
+            result.append(item)
+            
+    return result
