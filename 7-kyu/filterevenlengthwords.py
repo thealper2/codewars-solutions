96d6d076f949e756b8c5f5d95c7e7fd173f7b85d
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 22:47:47 +0300

    filterEvenLengthWords

---
+++ 7-kyu/filterevenlengthwords.py
@@ -0,0 +1,8 @@
+def filter_even_length_words(words):
+    result = []
+    for word in words:
+        if len(word) % 2 == 0:
+            result.append(word)
+            
+    return result
+        
