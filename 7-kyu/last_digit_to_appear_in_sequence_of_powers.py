de5157344763e39c33010a15f851fc0e39897290
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 12:21:03 +0300

    Last Digit to Appear in Sequence of Powers

---
+++ 7-kyu/last_digit_to_appear_in_sequence_of_powers.py
@@ -0,0 +1,18 @@
+def ldta(n):
+    seen = set()
+    counter = 0
+    power = 1
+    
+    while counter < 100:
+        total = n ** power
+        digits = list(map(int, list(str(total))))
+        
+        for digit in digits:
+            if digit not in seen:
+                if len(seen) == 9:
+                    return digit
+                else:
+                    seen.add(digit)
+        
+        counter += 1
+        power += 1
