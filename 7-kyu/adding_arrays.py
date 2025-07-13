510234eb8f9f295eb9a66fea81d0e457d971af10
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 13:02:06 +0300

    Adding Arrays

---
+++ 7-kyu/adding_arrays.py
@@ -0,0 +1,10 @@
+def arr_adder(arr):
+    m, n = len(arr), len(arr[0])
+    result = [[] for _ in range(n)]
+    for i in range(m):
+        for j in range(n):
+            if arr[i][j] != '':
+                result[j].append(arr[i][j])
+        
+    return " ".join(["".join(word) for word in result])
+    
