04f3b28747b74a2ab3a48a3d09276c63374237fa
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 21:37:22 +0300

    sum2total

---
+++ 7-kyu/sum2total.py
@@ -0,0 +1,9 @@
+def total(arr):
+    while len(arr) > 1:
+        temp = []
+        for i in range(len(arr) - 1):
+            temp.append(arr[i] + arr[i + 1])
+            
+        arr = temp
+        
+    return arr[0]
