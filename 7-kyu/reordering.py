7e0d8cf518ba3c30cc64b04c26834761e35cd815
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 12:29:37 +0300

    ReOrdering

---
+++ 7-kyu/reordering.py
@@ -0,0 +1,10 @@
+def re_ordering(text):
+    words = text.split()
+    result = []
+    for word in words:
+        if word[0].isupper():
+            result.insert(0, word)
+        else:
+            result.append(word)
+            
+    return " ".join(result)
