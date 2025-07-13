ec73c28ecc169591d667b0f6c545d9039ebba349
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 21:30:29 +0300

    Offload your work!

---
+++ 7-kyu/offload_your_work.py
@@ -0,0 +1,12 @@
+def work_needed(project_minutes, free_lancers):
+    total_minutes = 0
+    for free_lancer in free_lancers:
+        total_minutes += free_lancer[0] * 60 + free_lancer[1]
+        
+    if total_minutes >= project_minutes:
+        return "Easy Money!"
+    else:
+        diff = project_minutes - total_minutes
+        need_hours = diff // 60
+        need_minutes = diff - need_hours * 60
+        return f"I need to work {need_hours} hour(s) and {need_minutes} minute(s)"
