e4dff992696f0a0e759aa379302314f2486d26ff
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 23:54:18 +0300

    Valid HK Phone Nubmer

---
+++ 7-kyu/valid_hk_phone_number.py
@@ -0,0 +1,8 @@
+import re
+
+
+def is_valid_HK_phone_number(number):
+    return bool(re.match(r'\d{4} \d{4}\Z', number))
+    
+def has_valid_HK_phone_number(number):
+    return bool(re.search(r'\d{4} \d{4}', number))
