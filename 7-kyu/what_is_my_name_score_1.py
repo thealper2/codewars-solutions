a95005d5e7d4e3a8060b9177502e996ac95f31cb
Alper Karaca <thisiseternity12@gmail.com>
2025-07-13 23:39:11 +0300

    What is my name score? #1

---
+++ 7-kyu/what_is_my_name_score_1.py
@@ -0,0 +1,13 @@
+from preloaded import alpha
+
+def name_score(name):
+    score = 0
+    for char in name.upper():
+        if char == ' ':
+            continue
+        for letters, value in alpha.items():
+            if char in letters:
+                score += value
+                break
+    
+    return {name: score}
