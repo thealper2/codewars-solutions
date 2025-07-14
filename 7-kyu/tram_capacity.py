65b7840a23d37018988fb430914b7629bb12ce34
Alper Karaca <thisiseternity12@gmail.com>
2025-07-14 22:29:10 +0300

    Tram Capacity

---
+++ 7-kyu/tram_capacity.py
@@ -0,0 +1,10 @@
+def tram(stops, descending, onboarding):
+    current_passengers = 0
+    max_capacity = 0
+    
+    for i in range(stops):
+        current_passengers -= descending[i]
+        current_passengers += onboarding[i]
+        max_capacity = max(max_capacity, current_passengers)
+    
+    return max_capacity
