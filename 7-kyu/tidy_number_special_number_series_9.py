86ac2f4fd85eac27b5fe3b3760f4bfe18939d6e5
Alper Karaca <thisiseternity12@gmail.com>
2025-07-14 22:34:43 +0300

    Tidy Number (Special Numbers Series #9)

---
+++ 7-kyu/tidy_number_special_number_series_9.py
@@ -0,0 +1,12 @@
+def tidyNumber(n):
+    i = 0
+    prev = None
+    while 10 ** i <= n:
+        d = n // 10 ** i % 10
+        if prev is not None and d > prev:
+            return False
+        
+        prev = d
+        i += 1
+    
+    return True
