089a64c62d1781dcf3e5fbcda5918cfd4c206bf0
Alper Karaca <thisiseternity12@gmail.com>
2025-07-15 23:00:35 +0300

    Sub-array elements sum

---
+++ 7-kyu/sub_array_elements_sum.py
@@ -0,0 +1,11 @@
+def elements_sum(arr, d=0):
+    n = len(arr)
+    total = 0
+    for i in range(n, 0, -1):
+        outer_index = n - i
+        if len(arr[outer_index]) > i - 1:
+            total += arr[outer_index][i - 1]
+        else:
+            total += d
+            
+    return total
