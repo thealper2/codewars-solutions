663fef4d7b6ed9837f27b3e5a809b2163f6c09ae
Alper Karaca <thisiseternity12@gmail.com>
2025-07-15 22:48:34 +0300

    Sum Tims Tables

---
+++ 7-kyu/sum_times_tables.py
@@ -0,0 +1,9 @@
+def sum_times_tables(table, a, b):
+    if not table:
+        return 0
+    
+    total = 0
+    for i in range(a, b + 1):
+        total += sum([i * t for t in table])
+        
+    return total
