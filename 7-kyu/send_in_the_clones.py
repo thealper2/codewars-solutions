643c1fd02a11224e137ea80c6ee3cb7abbd941a6
Alper Karaca <thisiseternity12@gmail.com>
2025-07-15 21:55:29 +0300

    Send in the Clones

---
+++ 7-kyu/send_in_the_clones.py
@@ -0,0 +1,11 @@
+def clonewars(kata_per_day):
+    if kata_per_day == 0:
+        return [1, 0]
+    
+    num_clones = 2 ** (kata_per_day - 1)
+    total_attacks = 0
+
+    for i in range(1, kata_per_day + 1):
+        total_attacks += i * (2 ** (kata_per_day - i))
+
+    return [num_clones, total_attacks]
