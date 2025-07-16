e6f60bd5716b3669bbe1b34dc40e954915c1bbc8
Alper Karaca <thisiseternity12@gmail.com>
2025-07-16 22:06:10 +0300

    Naughty number

---
+++ 7-kyu/naughty_number.py
@@ -0,0 +1,16 @@
+def naughty_number(arr):
+    for index, sub_array in enumerate(arr):
+        stack = sub_array.copy()
+        found = None
+        while stack:
+            current = stack.pop()
+            if isinstance(current, list):
+                stack.extend(current)
+            else:
+                found = current
+                break
+
+        if found is not None:
+            return index
+        
+    return -1
