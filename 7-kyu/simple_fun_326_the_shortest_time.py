2d2f6510dffacbb4539ab1b4c53576c813b8d41e
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 15:24:49 +0300

    Simple Fun #326: The Shortest Time

---
+++ 7-kyu/simple_fun_326_the_shortest_time.py
@@ -0,0 +1,8 @@
+def shortest_time(n, m, speeds):
+    if n == 1:
+        return 0
+    
+    a, b, c, d = speeds
+    elevator_time = (abs(m - n) * a) + b + c + ((n - 1) * a) + b
+    walk_time = (n - 1) * d
+    return min(elevator_time, walk_time)
