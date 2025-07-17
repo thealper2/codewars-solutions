6a9a569743e03d6b69177b56a0aceff3880db604
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 11:34:41 +0300

    Some Circles

---
+++ 7-kyu/some_circles.py
@@ -0,0 +1,11 @@
+import math
+
+
+def sum_circles(*args):
+    total_area = 0
+    for d in args:
+        radius = d / 2
+        area = math.pi * (radius ** 2)
+        total_area += area
+        
+    return f"We have this much circle: {round(total_area)}"
