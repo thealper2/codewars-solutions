4e86a3003a1a219cc11d3d85bf05d8c6614d3169
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 14:31:50 +0300

    Simple Maths Test

---
+++ 7-kyu/simple_maths_test.py
@@ -0,0 +1,14 @@
+def number_property(n):
+    def is_prime(num):
+            if num < 2:
+                return False
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+
+    return [
+        is_prime(n),
+        n % 2 == 0,
+        n % 10 == 0
+    ]
