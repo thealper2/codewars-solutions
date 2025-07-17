0ba07ab43ef9740d77ef92889f19acf544ea2532
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 11:50:36 +0300

    Sine,cosine and others

---
+++ 7-kyu/sine_cosine_and_others.py
@@ -0,0 +1,28 @@
+import math
+
+
+def sctc(sin):
+    if not -1 <= sin <= 1:
+        return []
+    
+    result = [round(sin, 2)]
+    
+    try:
+        cos = math.sqrt(1 - sin ** 2)
+        result.append(round(cos, 2))
+    except:
+        return result
+    
+    try:
+        tan = sin / cos
+        result.append(round(tan, 2))
+    except ZeroDivisionError:
+        pass
+    
+    try:
+        cot = cos / sin
+        result.append(round(cot, 2))
+    except ZeroDivisionError:
+        pass
+    
+    return result
