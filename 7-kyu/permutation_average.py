5144383e6cc93681777882ff0895f127790c6ca7
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 16:45:25 +0300

    Permutation Average

---
+++ 7-kyu/permutation_average.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+
+def permutation_average(n):
+    digits = list(str(n))
+    unique_perms = set(permutations(digits))
+    total = 0
+    count = 0
+    for perm in unique_perms:
+        num = int(''.join(perm))
+        total += num
+        count += 1
+        
+    average = total / count
+    return round(average)
