86742ea285b47a4413fad69152b429be9fe209ca
Alper Karaca <thisiseternity12@gmail.com>
2025-07-17 23:52:03 +0300

    Simple Fun #177: Rank Of Element

---
+++ 7-kyu/simple_fun_177_rank_of_element.py
@@ -0,0 +1,13 @@
+def rank_of_element(arr, idx):
+    n = len(arr)
+    rank = 0
+    
+    for i in range(n):
+        if i < idx and arr[i] <= arr[idx]:
+            rank += 1
+        elif i == idx:
+            continue
+        elif i > idx and arr[i] < arr[idx]:
+            rank += 1
+            
+    return rank
