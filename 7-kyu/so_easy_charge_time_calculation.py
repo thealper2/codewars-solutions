d075ac58a1bbefac5e39a67a4fb754daa06431e8
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 11:40:11 +0300

    So Easy: Charge time calculation

---
+++ 7-kyu/so_easy_charge_time_calculation.py
@@ -0,0 +1,5 @@
+def calculate_time(battery,charger):
+    fast_charge = (battery * 0.85) / charger
+    decreasing_charge = (battery * 0.1) / (charger * 0.5)
+    trickle_charge = (battery * 0.05) / (charger * 0.2)
+    return round(fast_charge + decreasing_charge + trickle_charge, 2)
