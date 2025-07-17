77df5efd1bec974cede2b16aaff730f4e060863e
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 15:42:28 +0300

    Reducing Problems - Bug Fixing #8

---
+++ 7-kyu/reducing_problems_bug_fixing_8.py
@@ -0,0 +1,8 @@
+from functools import reduce
+
+
+def calculate_total(t1, t2):
+    t1_score = reduce(lambda a, b: a + b, t1, 0) if t1 else 0
+    t2_score = reduce(lambda a, b: a + b, t2, 0) if t2 else 0
+    return t1_score > t2_score
+    
