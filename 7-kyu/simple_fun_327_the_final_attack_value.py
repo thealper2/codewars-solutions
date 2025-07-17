54399c750af3f383c6e11041c237144324866b5b
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 15:18:57 +0300

    Simple Fun #327: The Final Attack Value

---
+++ 7-kyu/simple_fun_327_the_final_attack_value.py
@@ -0,0 +1,12 @@
+import math
+
+
+def final_attack_value(x, monster_list):
+    current_attack = x
+    for defense in monster_list:
+        if defense <= current_attack:
+            current_attack += defense
+        else:
+            current_attack += math.gcd(defense, current_attack)
+            
+    return current_attack
