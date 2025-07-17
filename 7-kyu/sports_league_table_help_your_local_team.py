54986df6f3d9e7df1f03a58b12f0f6b69b6e2c3b
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 11:21:03 +0300

    Sports league table - help your local team!

---
+++ 7-kyu/sports_league_table_help_your_local_team.py
@@ -0,0 +1,16 @@
+def league_calculate(team1, team2, result, league_table):
+    if result == "win":
+        points1, points2 = 3, 0
+    elif result == "draw":
+        points1 = points2 = 1
+    else:
+        points1 = points2 = 0
+    
+    for team in league_table:
+        if team[0] == team1:
+            team[1] += points1
+        if team[0] == team2:
+            team[1] += points2
+    
+    league_table.sort(key=lambda x: (-x[1], x[0]))
+    return league_table
