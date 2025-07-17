46d4fffcba347192a08353f202b2300fd380a027
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-17 10:42:12 +0300

    Jumping Number (Special Numbers Series #4)

---
+++ 7-kyu/jumping_number_special_numbers_series_4.py
@@ -0,0 +1,12 @@
+def jumping_number(number):
+    i = 0
+    prev = None
+    while 10 ** i <= number:
+        d = number // 10 ** i % 10
+        if prev is not None and abs(d - prev) != 1:
+            return "Not!!"
+
+        prev = d
+        i += 1
+
+    return "Jumping!!"
