6dc807cd63b5a87d8f2be53a235f6f2f476335ac
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 13:50:27 +0300

    Maximum Multiple

---
+++ 7-kyu/maximum_multiple.py
@@ -0,0 +1,15 @@
+def max_multiple(divisor, bound):
+    result = 0
+    for n in range(0, bound + 1):
+        if n % divisor != 0:
+            continue
+
+        if n > bound:
+            continue
+
+        if n < 0:
+            continue
+    
+        result = n
+        
+    return result
