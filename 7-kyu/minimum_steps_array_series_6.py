4ba58e40081db1aa720f8a2790722debb894dfd3
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 17:18:21 +0300

    Minimum Steps (Array Series #6)

---
+++ 7-kyu/minimum_steps_array_series_6.py
@@ -0,0 +1,11 @@
+def minimum_steps(numbers, value):
+    numbers.sort()
+    sum_ = 0
+    operations = -1
+    for num in numbers:
+        sum_ += num
+        operations += 1
+        if sum_ >= value:
+            return operations
+        
+    return -1
