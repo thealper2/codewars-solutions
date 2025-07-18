ec89d3c870c6517e845a9b7bf01b88d0e0ad083e
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 10:30:57 +0300

    RuplesJS #3: String EachChar

---
+++ 7-kyu/ruplesjs_3_string_eachchar.py
@@ -0,0 +1,12 @@
+def each_char(string, arg):
+    n = len(string)
+    result = ""
+    
+    for i in range(n):
+        if type(arg) == str:
+            result += string[i]
+            result += arg
+        else:
+            result += arg(string[i])
+    
+    return result
