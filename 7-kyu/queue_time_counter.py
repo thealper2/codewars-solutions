8ea2d8e474503c1e9161415a7e7958b51e556daf
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 10:54:42 +0300

    Queue time counter

---
+++ 7-kyu/queue_time_counter.py
@@ -0,0 +1,19 @@
+def queue(queuers, pos):
+    time = 0
+    
+    while True:
+        current = queuers.pop(0)
+        time += 1
+        current -= 1
+        
+        if pos == 0:
+            if current == 0:
+                return time
+            else:
+                queuers.append(current)
+                pos = len(queuers) - 1
+        else:
+            if current > 0:
+                queuers.append(current)
+                
+            pos -= 1
