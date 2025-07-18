26920a904c209814d3d3a1eb305d804b7cff1490
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 15:13:48 +0300

    Mutate My Strings

---
+++ 7-kyu/mutate_my_strings.py
@@ -0,0 +1,14 @@
+def mutate_my_strings(s1,s2):
+    if s1 == s2:
+        return s1 + '\n'
+    
+    result = [s1]
+    current = list(s1)
+    target = list(s2)
+    
+    for i in range(len(current)):
+        if current[i] != target[i]:
+            current[i] = target[i]
+            result.append(''.join(current))
+    
+    return '\n'.join(result) + '\n'
