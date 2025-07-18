00f9d0fde7bb9d5a67d66fb74c32a09489179e30
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 11:10:36 +0300

    Reverse and invert

---
+++ 7-kyu/reverse_and_invert.py
@@ -0,0 +1,14 @@
+def reverse_invert(lst):
+    result = []
+    for item in lst:
+        if type(item) == int:
+            item_str = str(item).replace('-', '')
+            item_str = item_str[::-1]
+            item_str = int(item_str)
+            
+            if item > 0:
+                result.append(-item_str)
+            else:
+                result.append(item_str)
+            
+    return result
