dcc9be1f132f4b2f28eaead304200b89b750b72c
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 10:43:52 +0300

    PopShift

---
+++ 7-kyu/popshift.py
@@ -0,0 +1,15 @@
+def pop_shift(s):
+    first = ""
+    second = ""
+    third = ""
+    l, r = 0, len(s) - 1
+    while l < r:
+        second += s[l]
+        first += s[r]
+        
+        l += 1
+        r -= 1
+        
+    if l == r:
+        third += s[l]
+    return [first, second, third]
