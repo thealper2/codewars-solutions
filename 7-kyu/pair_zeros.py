2dc1f9af574c802b7da52e43ae65469b31735e27
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 09:11:23 +0300

    Pair Zeros

---
+++ 7-kyu/pair_zeros.py
@@ -0,0 +1,11 @@
+def pair_zeros(arr):
+    zero_count = 0
+    result = []
+    for num in arr:
+        if num == 0:
+            zero_count += 1
+            if zero_count % 2 == 0:
+                continue
+                
+        result.append(num)
+    return result
