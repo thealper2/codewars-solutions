8c7192c412c7ced919934a454648c08c1260ddfb
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-18 17:07:55 +0300

    Mythical Heads and Tails

---
+++ 7-kyu/mythical_heads_and_tails.py
@@ -0,0 +1,12 @@
+def beasts(heads, tails):
+    numerator = heads - 2 * tails
+    if numerator % 3 != 0:
+        return "No solutions"
+    
+    h = numerator // 3
+    o = tails - h
+    
+    if h < 0 or o < 0:
+        return "No solutions"
+
+    return [o, h]
