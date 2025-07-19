dddfee2ab903549e35c32adc20af80050861a784
Alper Karaca <thisiseternity12@gmail.com>
2025-07-19 18:51:01 +0300

    Invalid Input - Error Handling #1

---
+++ 7-kyu/invalid_input_error_handling_1.py
@@ -0,0 +1,15 @@
+def get_count(words=''):
+    result = {'vowels': 0, 'consonants': 0}
+    if not words:
+        return result
+    
+    if type(words) == str:
+        for c in words:
+            if c == ' ':
+                continue
+            elif c.lower() in 'aeiou':
+                result['vowels'] += 1
+            elif not c.isdigit() and c.isalpha():
+                result['consonants'] += 1
+            
+    return result
