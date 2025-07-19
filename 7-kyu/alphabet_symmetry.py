77ec13e2355411320fc264244c6f66d0b59b9ccb
Alper Karaca <thisiseternity12@gmail.com>
2025-07-19 20:42:18 +0300

    Alphabet symmetry

---
+++ 7-kyu/alphabet_symmetry.py
@@ -0,0 +1,10 @@
+def solve(strings : list[str]) -> list[int]:
+    result = []
+    for word in strings:
+        count = 0
+        for index, char in enumerate(word.lower(), start=1):
+            if ord(char) - ord('a') + 1 == index:
+                count += 1
+                
+        result.append(count)
+    return result
