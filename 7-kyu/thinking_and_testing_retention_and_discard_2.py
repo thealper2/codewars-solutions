b703f56f8598395a387a708dc954b72203519953
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 16:26:40 +0300

    Thinking & Testing: Retention and discard 2

---
+++ 7-kyu/thinking_and_testing_retention_and_discard_2.py
@@ -0,0 +1,8 @@
+def mystery(s, n):
+    bin_n = []
+    while n > 0:
+        bin_n.append(str(n % 2))
+        n //= 2
+    
+    n = ''.join(bin_n[::-1]) 
+    return ''.join(y for x, y in zip(n, s) if x == '1')
