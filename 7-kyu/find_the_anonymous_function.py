ab688239e4805f9c4cb20f25a5d7a75049c40fc3
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 16:41:42 +0300

    Find the anonymous function

---
+++ 7-kyu/find_the_anonymous_function.py
@@ -0,0 +1,11 @@
+def find_function(func,arr):
+    predicate = None
+    for item in func:
+        if callable(item):
+            predicate = item
+            break
+            
+    if not predicate:
+        return []
+    
+    return [x for x in arr if predicate(x)]
