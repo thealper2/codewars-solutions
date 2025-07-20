b535ec29a9078cad3012fcdd1684d74798873a3f
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 20:56:49 +0300

    Can you count loop's execution?

---
+++ 7-kyu/can_you_count_loops_execution.py
@@ -0,0 +1,12 @@
+def count_loop_iterations(arr):
+    evaluations = []
+    product = 1
+    for v, inclusive in arr:
+        if inclusive:
+            current = v + 2
+        else:
+            current = v + 1
+        evaluations.append(product * current)
+        product *= (current - 1)
+        
+    return evaluations
