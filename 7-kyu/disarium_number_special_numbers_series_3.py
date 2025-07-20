8cf94f98a174e0c3bb44620eeba0642e65a0490b
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 21:21:57 +0300

    Disarium Number (Special Numbers Series #3)

---
+++ 7-kyu/disarium_number_special_numbers_series_3.py
@@ -0,0 +1,16 @@
+def disarium_number(number):
+    n = 0
+    while 10 ** n <= number:
+        n += 1
+        
+    i = 0
+    result = 0
+    while 10 ** i <= number:
+        d = number // 10 ** i % 10
+        
+        result += d ** n
+        i += 1
+        n -= 1
+        
+        
+    return "Disarium !!" if result == number else "Not !!"
