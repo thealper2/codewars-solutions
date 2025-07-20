1b33a934e189673760afc3605415f92066c523f7
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 13:05:00 +0300

    Blowing Birthday Candles

---
+++ 7-kyu/blowing_birthday_candles.py
@@ -0,0 +1,20 @@
+def blow_candles(s):
+    moves = 0
+    s = list(s)
+    n = len(s)
+    while True:
+        if all(c == '0' for c in s):
+            break
+
+        left = 0
+        while left < n and s[left] == '0':
+            left += 1
+
+        length = min(3, n - left)
+        for i in range(left, left + length):
+            if s[i] != '0':
+                s[i] = str(int(s[i]) - 1)
+
+        moves += 1
+    
+    return moves
