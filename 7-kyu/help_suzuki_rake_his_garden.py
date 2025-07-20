66e2503926310bb3f719bb7b2e53304595d6a397
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 13:31:07 +0300

    Help Suzuki rake his garden!

---
+++ 7-kyu/help_suzuki_rake_his_garden.py
@@ -0,0 +1,10 @@
+def rake_garden(garden):
+    items = garden.split()
+    result = []
+    for item in items:
+        if item == 'rock' or item == 'gravel':
+            result.append(item)
+        else:
+            result.append('gravel')
+            
+    return ' '.join(result)
