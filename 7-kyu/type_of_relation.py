b5b7fe541731312d0f08b2e6f202ca0c36fed0f7
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 14:06:31 +0300

    Type Of Relation

---
+++ 7-kyu/type_of_relation.py
@@ -0,0 +1,30 @@
+def type_of_function(domain, codomain, relations):
+    domain_to_codomain = {}
+    for x, y in relations:
+        if x in domain_to_codomain:
+            if domain_to_codomain[x] != y:
+                return 'It is not a function'
+        else:
+            domain_to_codomain[x] = y
+            
+    if set(domain_to_codomain.keys()) != domain:
+        return 'It is not a function'
+    
+    codomain_elements = set()
+    injective = True
+    for y in domain_to_codomain.values():
+        if y in codomain_elements:
+            injective = False
+            break
+        codomain_elements.add(y)
+            
+    surjective = set(domain_to_codomain.values()) == codomain
+    
+    if injective and surjective:
+        return "Bijective"
+    elif injective:
+        return "Injective"
+    elif surjective:
+        return "Surjective"
+    else:
+        return "General function"
