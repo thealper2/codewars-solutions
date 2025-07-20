7e6ef1f5a6c1e2bb17eb917117950f27745e5aa7
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 13:06:40 +0300

    International Morse Code Encryption

---
+++ 7-kyu/international_morse_code_encryption.py
@@ -0,0 +1,11 @@
+from preloaded import CHAR_TO_MORSE
+
+def encryption(string):
+    result = []
+    for c in string:
+        if c != ' ':
+            result.append(CHAR_TO_MORSE[c])        
+        else:
+            result.append(c)
+            
+    return " ".join(result)
