1128eac733e95ab76185792c42d13c889f46f083
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 21:36:12 +0300

    Simple Fun #173: A Hero Go To The School

---
+++ 7-kyu/simple_fun_173_a_hero_go_to_the_school.py
@@ -0,0 +1,12 @@
+def which_bus_to_take(buses_colors, going_to_school):
+    for i in range(len(buses_colors)):
+        if going_to_school[i]:
+            if buses_colors[i] == 'red':
+                return i
+            
+    for i in range(len(buses_colors)):
+        if going_to_school[i]:
+            if buses_colors[i] == 'blue':
+                return i
+    
+    return -1
