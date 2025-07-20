0c2bdb305fbafe8994c8d9b3da5499f8104ae25d
Alper Karaca <thisiseternity12@gmail.com>
2025-07-20 19:16:58 +0300

    Find an employees role in the company

---
+++ 7-kyu/find_an_employees_role_in_the_company.py
@@ -0,0 +1,7 @@
+def find_employees_role(name):
+    for employee in employees:
+        employee_name = f"{employee['first_name']} {employee['last_name']}"
+        if employee_name == name:
+            return employee['role']
+    
+    return "Does not work here!"
