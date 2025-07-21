8c7b8b1a85339119facb5192753ad37ad4559b62
Alper Karaca <thisiseternity12@gmail.com>
2025-07-21 20:26:42 +0300

    Simple Fun #12: Count Sum of Two Representations

---
+++ 7-kyu/simple_fun_12_count_sum_of_two_representations.py
@@ -0,0 +1,8 @@
+def count_sum_of_two_representations(n, l, r):
+    count = 0
+    for A in range(l, r + 1):
+        B = n - A
+        if l <= B <= r and A <= B:
+            count += 1
+            
+    return count
