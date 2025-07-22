2803fdcd8c8c1d95ed4eb5c549a9d9c077198f87
Alper Karaca <thisiseternity12@gmail.com>
2025-07-22 21:26:09 +0300

    Conference Traveller

---
+++ 7-kyu/conference_traveller.py
@@ -0,0 +1,8 @@
+def conference_picker(cities_visited, cities_offered):
+    for city in cities_offered:
+        if city in cities_visited:
+            continue
+        else:
+            return city
+        
+    return "No worthwhile conferences this year!"
