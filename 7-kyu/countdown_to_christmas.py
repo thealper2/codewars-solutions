1d967b42f9efd25d7888dd071bc58c24e6845f19
Alper Karaca <thisiseternity12@gmail.com>
2025-07-22 21:16:04 +0300

    Countdown to Christmas

---
+++ 7-kyu/countdown_to_christmas.py
@@ -0,0 +1,11 @@
+import datetime
+
+def days_until_christmas(current_date):
+    year = current_date.year
+    christmas = datetime.date(year, 12, 25)
+    delta = christmas - current_date
+    if delta.days < 0:
+        christmas = datetime.date(year + 1, 12, 25)
+        delta = christmas - current_date
+    
+    return delta.days
