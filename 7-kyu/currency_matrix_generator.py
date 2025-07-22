1094984456131649a3cc80aab37c9e59a683883a
Alper Karaca <thisiseternity12@gmail.com>
2025-07-22 20:48:53 +0300

    Currency Matrix Generator

---
+++ 7-kyu/currency_matrix_generator.py
@@ -0,0 +1,17 @@
+def generate_currency_matrix(currency):
+    currency_strength = ["EUR", "GBP", "AUD", "NZD", "USD", "CAD", "CHF", "JPY"]
+    n = len(currency_strength)
+    result = []
+    found = False
+    
+    for i in range(n):
+        if currency_strength[i] == currency:
+            found = True
+            
+        elif found:
+            result.append(currency + currency_strength[i])
+        
+        else:
+            result.append(currency_strength[i] + currency)
+            
+    return result
