7e5732985c04364b156674a53bba115593c60a84
Alper Karaca <thisiseternity12@gmail.com>
2025-07-23 20:39:39 +0300

    Coding 3min: toLoverCase()

---
+++ 7-kyu/coding_3_min_tolovercase.py
@@ -0,0 +1,10 @@
+def to_lover_case(string):
+    result = ""
+    for c in string:
+        if c.isalpha():
+            c_ord = (ord(c.upper()) - ord('A')) % 4
+            result += ['L', 'O', 'V', 'E'][c_ord]
+        else:
+            result += c
+            
+    return "".join(result)
