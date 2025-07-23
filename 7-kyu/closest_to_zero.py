ab36501701d5474eb21ae578706e3aed700cad2e
Alper Karaca <thisiseternity12@gmail.com>
2025-07-23 20:53:20 +0300

    Closest to Zero

---
+++ 7-kyu/closest_to_zero.py
@@ -0,0 +1,18 @@
+def closest(lst):
+    closest = None
+    min_distance = float('inf')
+    
+    for num in lst:
+        distance = abs(num)
+        
+        if distance == 0:
+            return 0
+        
+        if distance < min_distance:
+            min_distance = distance
+            closest = num
+        elif distance == min_distance:
+            if num != closest:
+                closest = None
+    
+    return closest
