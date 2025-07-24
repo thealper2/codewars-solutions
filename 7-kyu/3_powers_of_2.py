be6e6b1a325b0064735c628305dab4bbe937064d
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 10:52:43 +0300

    3 powers of 2

---
+++ 7-kyu/3_powers_of_2.py
@@ -0,0 +1,10 @@
+def three_powers(n):
+    if n < 3:
+        return False
+    
+    count = 0
+    while n > 0:
+        if n % 2 == 1:
+            count += 1
+        n = n // 2
+    return count <= 3
