088c4b965d4c7085646a6b2179efb782debeab1a
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 14:50:52 +0300

    Array Manipulation

---
+++ 7-kyu/array_manipulation.py
@@ -0,0 +1,10 @@
+def array_manip(array):
+    n = len(array)
+    for i in range(n):
+        right_elements = array[i+1:]
+        candidates = [x for x in right_elements if x > array[i]]
+        if candidates:
+            array[i] = min(candidates)
+        else:
+            array[i] = -1
+    return array
