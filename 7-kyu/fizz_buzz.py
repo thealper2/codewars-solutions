45b3467fec74846159b200ba301fb4e652d971d3
Alper Karaca <thisiseternity12@gmail.com>
2025-07-24 23:27:56 +0300

    Fizz Buzz

---
+++ 7-kyu/fizz_buzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            result.append('FizzBuzz')
+        elif i % 5 == 0:
+            result.append('Buzz')
+        elif i % 3 == 0:
+            result.append('Fizz')
+        else:
+            result.append(i)
+    
+    return result
