ad8092b56a48f622eab059265a2a9e986f540268
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 11:12:54 +0300

    Most-1-bits

---
+++ 7-kyu/most_1_bits.py
@@ -0,0 +1,17 @@
+def most_one_bits(nums:list[int]) -> int:
+    seen = set()
+    max_count = -1
+    result = None
+    for num in nums:
+        if num in seen:
+            continue
+        seen.add(num)
+        if num < 0:
+            binary = bin(num & 0xFF)[2:].zfill(8)
+        else:
+            binary = bin(num)[2:].zfill(8)[-8:]
+        count = binary.count('1')
+        if count >= max_count:
+            max_count = count
+            result = numa
+    return result
