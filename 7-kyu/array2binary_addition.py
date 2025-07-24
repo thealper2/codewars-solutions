f3038ab6d12cd40348f3e830add9a259b8e46e41
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 11:14:49 +0300

    Array2Binary addition

---
+++ 7-kyu/array2binary_addition.py
@@ -0,0 +1,9 @@
+def arr2bin(arr):
+    result = 0
+    for item in arr:
+        if type(item) != int:
+            return False
+        
+        result += item
+        
+    return str(bin(result))[2:]
