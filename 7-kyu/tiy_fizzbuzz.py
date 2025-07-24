7a807965bb94ba4d027f96f0209547104c4ddef0
Alper Karaca <thisiseternity12@gmail.com>
2025-07-24 21:08:35 +0300

    TIY-FizzBuzz

---
+++ 7-kyu/tiy_fizzbuzz.py
@@ -0,0 +1,17 @@
+def tiy_fizz_buzz(string):
+    lower_vowels = 'aeiou'
+    upper_vowels = 'AEIOU'
+    result = ''
+    for c in string:
+        if c in lower_vowels:
+            result += 'Yard'
+        elif c in upper_vowels:
+            result += 'Iron Yard'
+        elif c.isupper() and c not in upper_vowels:
+            result +=  'Iron'
+        elif c.islower() and c not in lower_vowels:
+            result +=  c
+        else:
+            result += c
+    
+    return result
