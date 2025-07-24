7eb70e3901e6cb0ff5540d9bbf06f5f9fd4ff4b9
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 10:46:09 +0300

    Sort the Bytes

---
+++ 7-kyu/sort_the_bytes.py
@@ -0,0 +1,9 @@
+def sort_bytes(n):
+    byte1 = (n >> 24) & 0xFF
+    byte2 = (n >> 16) & 0xFF
+    byte3 = (n >> 8) & 0xFF
+    byte4 = n & 0xFF
+    bytes_list = [byte1, byte2, byte3, byte4]
+    bytes_list.sort(reverse=True)
+    new_number = (bytes_list[0] << 24) | (bytes_list[1] << 16) | (bytes_list[2] << 8) | bytes_list[3]
+    return new_number
