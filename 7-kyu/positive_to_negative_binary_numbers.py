af432305f444fe03365770e189d9dc0c3620efca
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-24 15:27:11 +0300

    Positive to negative binary numbers

---
+++ 7-kyu/positive_to_negative_binary_numbers.py
@@ -0,0 +1,19 @@
+def positive_to_negative(binary):
+    n = len(binary)
+    if not binary:
+        return []
+    
+    inverted = [1 - bit for bit in binary]
+    carry = 1
+    result = []
+    for bit in reversed(inverted):
+        sum_bit = bit + carry
+        if sum_bit == 2:
+            result.append(0)
+            carry = 1
+        else:
+            result.append(sum_bit)
+            carry = 0
+
+    result.reverse()
+    return result
