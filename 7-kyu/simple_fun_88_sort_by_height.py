2a5352357bfa44b2c8e4941ea1c434cb5cc4277b
Alper Karaca <thisiseternity12@gmail.com>
2025-07-24 23:23:02 +0300

    Simple Fun #88: Sort By Height

---
+++ 7-kyu/simple_fun_88_sort_by_height.py
@@ -0,0 +1,12 @@
+def sort_by_height(a):
+    n = len(a)
+    trees = [h for h in a if h != -1]
+    trees.sort()
+    people_index = 0
+    
+    for i in range(n):
+        if a[i] != -1:
+            a[i] = trees[people_index]
+            people_index += 1
+    
+    return a
