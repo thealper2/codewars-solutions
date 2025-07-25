d10b0d1438c81877e1737459a5a4e74b78df0f54
Alper Karaca <thisiseternity12@gmail.com>
2025-07-25 23:58:39 +0300

    Floating-point Approximation (III)

---
+++ 7-kyu/floating_point_approximation_3.py
@@ -0,0 +1,13 @@
+import math
+
+
+def quadratic(a, b, c):
+    discriminant = b**2 - 4 * a * c
+    sqrt_discriminant = math.sqrt(discriminant)
+    
+    if b > 0:
+        x2 = (2 * c) / (-b - sqrt_discriminant)
+    else:
+        x2 = (2 * c) / (-b + sqrt_discriminant)
+    
+    return x2
