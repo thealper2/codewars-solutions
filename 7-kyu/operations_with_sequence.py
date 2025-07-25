7c5a912d74741bf4af65d6831f8e41c14d8d0567
Alper Karaca <thisiseternity12@gmail.com>
2025-07-25 23:33:16 +0300

    Operations with sequence

---
+++ 7-kyu/operations_with_sequence.py
@@ -0,0 +1,17 @@
+def calc(arr):
+    n = len(arr)
+    processed = []
+    for i in range(n):
+        num = arr[i]
+        if num > 0:
+            num = num ** 2
+
+        if (i + 1) % 3 == 0:
+            num *= 3
+
+        if (i + 1) % 5 == 0:
+            num *= -1
+
+        processed.append(num)
+    
+    return sum(processed)
