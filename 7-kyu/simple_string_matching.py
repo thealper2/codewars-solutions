8b9760232f864a44470a6dad35490abde9672e80
Alper Karaca <thisiseternity12@gmail.com>
2025-07-26 17:47:00 +0300

    Simple string matching

---
+++ 7-kyu/simple_string_matching.py
@@ -0,0 +1,19 @@
+def solve(a, b):
+    asterisk_index = a.find('*')
+    
+    if asterisk_index == -1:
+        return a == b
+    else:
+        prefix = a[:asterisk_index]
+        suffix = a[asterisk_index + 1:]
+        
+        if len(prefix) + len(suffix) > len(b):
+            return False
+        
+        if not b.startswith(prefix):
+            return False
+        
+        if not b.endswith(suffix):
+            return False
+        
+        return True
