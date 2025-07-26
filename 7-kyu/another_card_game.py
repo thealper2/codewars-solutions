39fd3acf849ec949fb832c24f5b54e2aec389c8b
Alper Karaca <thisiseternity12@gmail.com>
2025-07-26 17:37:25 +0300

    Another card game

---
+++ 7-kyu/another_card_game.py
@@ -0,0 +1,13 @@
+def solution(frank, sam, tom):  
+    count = 0
+    for i in range(4):
+        for j in range(4):
+            if frank[j] > sam[i] and frank[j] > tom[i]:
+                count += 1
+                frank[j] = 0
+                break
+                
+            if count == 2:
+                return True
+    
+    return False
