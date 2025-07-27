bbee83b8a9b7f104381d6967cade4b6e6a2ce04f
Alper Karaca <thisiseternity12@gmail.com>
2025-07-27 23:53:49 +0300

    Log without dates

---
+++ 7-kyu/log_without_dates.py
@@ -0,0 +1,22 @@
+def check_logs(log):
+    if not log:
+        return 0
+    
+    n = len(log)
+    count = 1
+    for i in range(1, n):
+        prev_time = log[i - 1].split(':')
+        current_time = log[i].split(':')
+        
+        prev_h, prev_m, prev_s = map(int, prev_time)
+        curr_h, curr_m, curr_s = map(int, current_time)
+        
+        if curr_h < prev_h:
+            count += 1
+        elif curr_h == prev_h:
+            if curr_m < prev_m:
+                count += 1
+            elif curr_m == prev_m and curr_s <= prev_s:
+                count += 1
+    
+    return count
