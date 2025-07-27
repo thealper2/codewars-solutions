4a817a2bf40d15ae970c874e925ec7c01ea3eeb4
Alper Karaca <thisiseternity12@gmail.com>
2025-07-27 23:21:04 +0300

    Password Check - Binary to String

---
+++ 7-kyu/password_check_binary_to_string.py
@@ -0,0 +1,9 @@
+def decode_pass(pass_list, bits):
+    password = ''
+    for bit in bits.split():
+        password += chr(int(bit, 2)) 
+        
+    if password in pass_list:
+        return password
+    
+    return False
