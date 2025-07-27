c3e62ab8e492845e7d2d6ccc65c29d6f185b077a
Alper Karaca <thisiseternity12@gmail.com>
2025-07-27 21:38:49 +0300

    Weird words

---
+++ 7-kyu/weird_words.py
@@ -0,0 +1,15 @@
+def next_letter(s):
+    result = []
+    for char in s:
+        if char.isalpha():
+            if char == 'z':
+                next_char = 'a'
+            elif char == 'Z':
+                next_char = 'A'
+            else:
+                next_char = chr(ord(char) + 1)
+            result.append(next_char)
+        else:
+            result.append(char)
+            
+    return ''.join(result)
