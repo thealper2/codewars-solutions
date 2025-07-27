a32233875f04afe22a4160fbc05ade5092685f7b
Alper Karaca <thisiseternity12@gmail.com>
2025-07-27 21:27:45 +0300

    Bob's Short Forms

---
+++ 7-kyu/bobs_short_forms.py
@@ -0,0 +1,10 @@
+def short_form(s):
+    result = ''
+    n = len(s)
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            result += s[i]
+        elif s[i] not in 'aeiouAEIOU':
+            result += s[i]
+            
+    return result
