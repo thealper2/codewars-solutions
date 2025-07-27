bb1c876002e260127014699fd5f94d6052ae53e4
Alper Karaca <thisiseternity12@gmail.com>
2025-07-27 23:15:54 +0300

    Sum and Multiply

---
+++ 7-kyu/sum_and_multiply.py
@@ -0,0 +1,7 @@
+def sum_and_multiply(sum, multiply):
+    for left in range(sum):
+        right = sum - left
+        if left + right == sum and left * right == multiply:
+            return [left, right]
+        
+    return None
