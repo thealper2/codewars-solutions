1356de092cb0e3b1d45a1966f4e4888b72927092
Alper Karaca <thisiseternity12@gmail.com>
2025-07-28 20:53:34 +0300

    Password System

---
+++ 7-kyu/password_system.py
@@ -0,0 +1,16 @@
+import math
+
+
+def help_zoom(grid):
+    length = len(grid)
+    n = math.isqrt(length)
+    if n * n != length:
+        return "No"
+    
+    for i in range(n):
+        for j in range(n):
+            sym_i = n - 1 - i
+            sym_j = n - 1 - j
+            if grid[i * n + j] != grid[sym_i * n + sym_j]:
+                return "No"
+    return "Yes"
