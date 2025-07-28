e01e7536bcd70a21bcba4d2a3b85c6c31955ee70
Alper Karaca <thisiseternity12@gmail.com>
2025-07-28 22:37:06 +0300

    Is Sator Square?

---
+++ 7-kyu/is_sator_square.py
@@ -0,0 +1,11 @@
+def is_sator_square(tablet):
+    n = len(tablet)
+    if n == 0:
+        return False
+    
+    for i in range(n):
+        for j in range(n):
+            if tablet[i][j] != tablet[j][i] or tablet[i][j] != tablet[n-1-i][n-1-j]:
+                return False
+    
+    return True
