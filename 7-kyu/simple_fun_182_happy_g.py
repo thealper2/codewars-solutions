bb7edc5bb90ce1582556927cbec8ad12f0891c9c
Alper Karaca <thisiseternity12@gmail.com>
2025-07-28 21:05:35 +0300

    Simple Fun #182: Happy 'g'

---
+++ 7-kyu/simple_fun_182_happy_g.py
@@ -0,0 +1,15 @@
+def happy_g(st):
+    n = len(st)
+    for i in range(n):
+        if st[i] == 'g':
+            if i == 0:
+                if st[i + 1] != 'g':
+                    return False
+            elif i == n - 1:
+                if st[i - 1] != 'g':
+                    return False
+                
+            elif st[i - 1] != 'g' and st[i + 1] != 'g':
+                return False
+            
+    return True
