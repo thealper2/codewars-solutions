e7881948d49611372b0ed50847dbaf63c75e0777
Alper Karaca <thisiseternity12@gmail.com>
2025-07-30 00:02:47 +0300

    Average Array

---
+++ 7-kyu/average_array.py
@@ -0,0 +1,9 @@
+def avg_array(arrs):
+    m, n = len(arrs), len(arrs[0])
+    averages = [0] * n
+    for i in range(m):
+        for j in range(n):
+            averages[j] += arrs[i][j]
+    
+    averages = [row / m for row in averages]
+    return averages
