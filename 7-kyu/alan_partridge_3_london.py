ab1a57f271e0fc25ecdd672bd36c9d5464e057c9
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-31 16:45:40 +0300

    Alan Partridge 3 - London

---
+++ 7-kyu/alan_partridge_3_london.py
@@ -0,0 +1,7 @@
+def alan(arr):
+    stops = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway']
+    for stop in stops:
+        if stop not in arr:
+            return 'No, seriously, run. You will miss it.'
+        
+    return 'Smell my cheese you mother!'
