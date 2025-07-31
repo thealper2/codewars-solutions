ace9b0232f7578474befbd37fa3dadda967f29ac
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-31 09:24:05 +0300

    Find Fibonacci last digit

---
+++ 7-kyu/find_fibonacci_last_digit.py
@@ -0,0 +1,12 @@
+def get_last_digit(index):
+    if index == 0:
+        return 0
+    
+    elif index == 1 or index == 2:
+        return 1
+    
+    a, b = 1, 1
+    for _ in range(3, index + 1):
+        a, b = b, (a + b) % 10
+    
+    return b
