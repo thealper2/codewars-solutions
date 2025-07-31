a565edfca864d044fefba1ab82f32bc5528c2086
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-07-31 09:13:40 +0300

    Complete The Pattern #7 - Cyclical Permutation

---
+++ 7-kyu/complete_the_pattern_7_cyclical_permutation.py
@@ -0,0 +1,14 @@
+def pattern(n):
+    if n <= 0:
+        return ''
+    
+    result = []
+    for i in range(1, n + 1):
+        p = ''.join([str(_) for _ in range(i, n + 1)])
+        for j in range(1, i):
+            p += str(j)
+            
+        result.append(p)
+        
+    return '\n'.join(result)
+            
