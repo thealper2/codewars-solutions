b1ae5eb046105fc09147bb25d4bcc1d6582e21d6
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-08-07 14:59:00 +0300

    Folding your way to the moon

---
+++ 7-kyu/folding_your_way_to_the_moon.py
@@ -0,0 +1,12 @@
+def fold_to(distance):
+    if distance < 0:
+        return None
+
+    thickness = 0.0001
+    folds = 0
+
+    while thickness < distance:
+        thickness *= 2
+        folds += 1
+
+    return folds
