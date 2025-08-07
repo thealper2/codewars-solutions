22bf31ecfab94cbf50e3d9f0efa77e04718d9894
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-08-07 16:36:47 +0300

    Calculate the resultant force

--- 7-kyu/calculate_the_resultant_force.py
+++ 7-kyu/calculate_the_resultant_force.py
@@ -1,16 +1,8 @@
 import math
 
-
 def solution(F1, F2, theta) :
     theta_rad = math.radians(theta)
     R = math.sqrt(F1**2 + F2**2 + 2 * F1 * F2 * math.cos(theta_rad))
-    if R == 0:
-        phi_deg = 0.0
-        
-    else:
-        sin_phi = (F2 * math.sin(theta_rad)) / R
-        sin_phi = min(1, max(-1, sin_phi))
-        phi_rad = math.asin(sin_phi)
-        phi_deg = math.degrees(phi_rad)
-        
+    phi = math.atan2(F2 * math.sin(theta_rad), F1 + F2 * math.cos(theta_rad))
+    phi_deg = math.degrees(phi)
     return [R, phi_deg]
