b59c346dc5c992a6c429f61bfae82c3c40f7ea2f
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-08-07 11:32:09 +0300

    Debug the functions EASY

---
+++ 7-kyu/debug_the_functions_easy.py
@@ -0,0 +1,11 @@
+def multi(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+def add(numbers):
+    return sum(numbers)
+
+def reverse(string):
+    return string[::-1]
