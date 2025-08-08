f20aacc444b187377e8d11e7bc6e25bba424b7ac
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-08-08 14:02:17 +0300

    Predict your age!

---
+++ 7-kyu/predict_your_age.py
@@ -0,0 +1,8 @@
+import math
+
+
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    ages = [age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8]
+    total = sum(age ** 2 for age in ages)
+    result = math.sqrt(total) / 2
+    return int(result)
