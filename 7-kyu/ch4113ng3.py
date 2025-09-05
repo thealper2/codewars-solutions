7371bf26d392d01dd26f0e4de926c94882f30ed4
Alper Karaca <thisiseternity12@gmail.com>
2025-09-05 23:28:25 +0300

    Ch4113ng3

---
+++ 7-kyu/ch4113ng3.py
@@ -0,0 +1,7 @@
+def nerdify(txt):
+    txt = txt.replace('a', '4')
+    txt = txt.replace('A', '4')
+    txt = txt.replace('e', '3')
+    txt = txt.replace('E', '3')
+    txt = txt.replace('l', '1')
+    return txt
