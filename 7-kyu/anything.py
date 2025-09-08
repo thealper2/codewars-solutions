9cb9bf42e154780ce5372ce69da2a01bd121e2f7
Alper Karaca <thisiseternity12@gmail.com>
2025-09-08 20:51:12 +0300

    Anything

---
+++ 7-kyu/anything.py
@@ -0,0 +1,21 @@
+class Anything:
+    def __lt__(self, other):
+        return True
+    
+    def __le__(self, other):
+        return True
+    
+    def __eq__(self, other):
+        return True
+    
+    def __ne__(self, other):
+        return True
+    
+    def __gt__(self, other):
+        return True
+    
+    def __ge__(self, other):
+        return True
+
+def anything(thing):
+    return Anything()
