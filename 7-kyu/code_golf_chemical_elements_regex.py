5b49bc1ab261b37ca6676d3bb477771c1a9c4c2b
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-09-11 16:18:46 +0300

    [Code Golf] Chemical Elements Regex

---
+++ 7-kyu/code_golf_chemical_elements_regex.py
@@ -0,0 +1,1 @@
+R = "([IUVW]|[ACE][rsu]|[BFKPS]r?|[CGN][ade]|[IZ][nr]|A[cglmt]|B[aehik]|C[fl-o]?|D[bsy]|F[elm]|[HO][gs]?|H[efo]|L[airuv]|M[cdgnot]|N[bhiop]?|P[abdmotu]|R[abe-hnu]|S[bcegimn]|T[abcehilms]|Xe|Yb?)$"
