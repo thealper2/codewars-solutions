bbc2bee4eb5c3c7d1b5fdcd3fd61ce6a0c9c2809
Alper Karaca <thisiseternity12@gmail.com>
2025-10-17 21:01:32 +0300

    Task master

--- 6-kyu/task_master.py
+++ 6-kyu/task_master.py
@@ -1,7 +1,23 @@
-from concurrent.futures import ThreadPoolExecutor
+import threading
+import queue
 
-def task_master(array):
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(lambda f: f(), array))
-        
-    return sum(results)
+def task_master(slow_functions):
+    result_queue = queue.Queue()
+    
+    def worker(func):
+        result_queue.put(func())
+    
+    threads = []
+    for func in slow_functions:
+        t = threading.Thread(target=worker, args=(func,))
+        t.start()
+        threads.append(t)
+    
+    for t in threads:
+        t.join()
+    
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    
+    return total
