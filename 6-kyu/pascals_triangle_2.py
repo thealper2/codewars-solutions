f604dabdce623c21d565b0fa1c8e9d7b758a9dc4
Alper Karaca <thisiseternity12@gmail.com>
2026-05-29 22:33:34 +0300

    Pascal's Triangle #2

---
+++ 6-kyu/pascals_triangle_2.py
@@ -0,0 +1,11 @@
+def pascal(p):
+    triangle = []
+    
+    for i in range(p):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            
+        triangle.append(row)
+        
+    return triangle
