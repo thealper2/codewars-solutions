ebd074f404e28a57e366e0b2f1432275f51ec381
Alper Karaca <thisiseternity12@gmail.com>
2026-05-29 23:56:56 +0300

    Roman Numerals Encoder

---
+++ 6-kyu/roman_numerals_encoder.py
@@ -0,0 +1,13 @@
+def solution(n):
+    result = ''
+    roman = {
+        1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL',
+        50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
+    }
+    
+    for value, symbol in sorted(roman.items(), reverse=True):
+        while n >= value:
+            result += symbol
+            n -= value
+            
+    return result
